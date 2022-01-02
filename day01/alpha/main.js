24124dc74a407b40314be578472ec316d784a495
Alpha <alphatero@icloud.com>
2022-01-02 16:59:15 +0800

    feat: 🎸 day one beginner

---
+++ day01/alpha/main.js
@@ -0,0 +1,22 @@
+import './style.css';
+
+/**
+ * 當按下鍵盤上關鍵字時會發出對應的音效以及帶有css效果
+ */
+
+//keydown better than keypress because keydown keycode is same between uppercase and lowercase
+document.addEventListener('keydown', function (e) {
+  const currentAudio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  //if keydown is not in keys then return
+  if (!currentAudio) return;
+  //play() is audio tag's function;
+  currentAudio.play();
+
+  //same about css style event
+  const currentKey = document.querySelector(`div[data-key="${e.keyCode}"]`);
+  if (!currentKey) return;
+  currentKey.classList.add('play');
+  setTimeout(() => {
+    currentKey.classList.remove('play');
+  }, 100);
+});
---
+++ day01/alpha/tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        roboto: ['Roboto', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+};
