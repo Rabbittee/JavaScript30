dcb2d3e1b0b7f5b2082a8be7779a1e1edf4be539
Alpha <alphatero@icloud.com>
2022-01-03 13:12:37 +0800

    feat: 🎸 add header component

--- day01/alpha/main.js
+++ day01/alpha/main.js
@@ -1,11 +1,12 @@
-import './style.css';
+import "./style.css";
+import "./src/header";
 
 /**
  * 當按下鍵盤上關鍵字時會發出對應的音效以及帶有css效果
  */
 
 //keydown better than keypress because keydown keycode is same between uppercase and lowercase
-document.addEventListener('keydown', function (e) {
+document.addEventListener("keydown", function (e) {
   const currentAudio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
   //if keydown is not in keys then return
   if (!currentAudio) return;
@@ -17,9 +18,9 @@ document.addEventListener('keydown', function (e) {
   //same about css style event
   const currentKey = document.querySelector(`div[data-key="${e.keyCode}"]`);
   if (!currentKey) return;
-  currentKey.classList.add('play');
+  currentKey.classList.add("play");
   //time out let the active effects off
   setTimeout(() => {
-    currentKey.classList.remove('play');
+    currentKey.classList.remove("play");
   }, 100);
 });
