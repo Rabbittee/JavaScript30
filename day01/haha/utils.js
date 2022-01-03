135ce9ce9512d53d9b577cfd707bf887397c3368
haha861924 <qq861924@gmail.com>
2022-01-03 14:28:01 +0800

    style: format

--- day01/haha/utils.js
+++ day01/haha/utils.js
@@ -1,19 +1,19 @@
 document.body.addEventListener("keydown", getKey, false);
 
-      function getKey(e) {
-        const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-        const target = document.querySelector(`div[data-key="${e.keyCode}"]`);
-        if (!audio) return false;
-        audio.play();
-        target.classList.add("playing");
-        target.addEventListener("transitionend", removeClass);
-      }
+function getKey(e) {
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const target = document.querySelector(`div[data-key="${e.keyCode}"]`);
+  if (!audio) return false;
+  audio.play();
+  target.classList.add("playing");
+  target.addEventListener("transitionend", removeClass);
+}
 
-      function removeClass() {
-        const keys = document.querySelectorAll(".key");
-        keys.forEach((el) => {
-          if (el.classList.contains("playing")) {
-            el.classList.remove("playing");
-          }
-        });
-      }
+function removeClass() {
+  const keys = document.querySelectorAll(".key");
+  keys.forEach((el) => {
+    if (el.classList.contains("playing")) {
+      el.classList.remove("playing");
+    }
+  });
+}
