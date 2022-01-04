df93ec59c33a583ac0c9f855cb002fd6fb375508
kayac-chang <egg734631@gmail.com>
2022-01-04 23:04:21 +0800

    feat: 🎸 add day05 kirby

---
+++ day05/kirby/script.ts
@@ -0,0 +1,7 @@
+const select = (query: string) => document.querySelector<HTMLElement>(query);
+
+select(".panels").addEventListener("click", ({ target }) => {
+  if (!(target instanceof Element)) return;
+
+  target.closest(".panel")?.classList.toggle("open");
+});
