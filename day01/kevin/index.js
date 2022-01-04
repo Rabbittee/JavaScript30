00ef950867c523499acd606d6d19b93091d2d897
kevin_hws <kevinshu1995@gmail.com>
2022-01-05 00:18:27 +0800

    feat: 🎸 day 1 kevin solution

---
+++ day01/kevin/index.js
@@ -0,0 +1,53 @@
+(function keyBoardDrum() {
+  document.addEventListener("keydown", keydownHandler);
+  document.addEventListener("keyup", keyupHandler);
+
+  // * 紀錄那些正在被觸發的 keycode
+  const activeKeys = new Set();
+
+  // * 按下去的時候
+  function keydownHandler(event) {
+    const keycode = getKeycode(event);
+    if (activeKeys.has(keycode)) return;
+
+    playAudio(keycode);
+    toggleActiveBoxClass(keycode, true);
+    activeKeys.add(keycode);
+  }
+
+  // * 放開的時候
+  function keyupHandler(event) {
+    const keycode = getKeycode(event);
+    activeKeys.delete(keycode);
+    toggleActiveBoxClass(keycode, false);
+  }
+
+  function getKeycode(event) {
+    return event.keyCode;
+  }
+
+  // * Audio relates functions =======
+  function getTargetAudio(keycode) {
+    return document.querySelector(`audio[data-key="${keycode}"]`);
+  }
+
+  function playAudio(keycode) {
+    const targetAudio = getTargetAudio(keycode);
+
+    if (targetAudio === null) return;
+    targetAudio.currentTime = 0;
+    targetAudio.play();
+  }
+
+  // * View relates functions ======
+  function toggleActiveBoxClass(keycode, isActive = false) {
+    const classListMethod = isActive ? "add" : "remove";
+    const activeClassName = "playing";
+    const targetBox = document.querySelector(`.key[data-key="${keycode}"]`);
+
+    if (targetBox === null) return;
+    targetBox.classList[classListMethod](activeClassName);
+  }
+})();
+
+const whyIIFE = "就說要用IIFE吧";
