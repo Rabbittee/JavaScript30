feb7dcecc4f2a57ba53327e26094a25e5a6cfacc
Alpha <alphatero@icloud.com>
2022-01-05 23:20:36 +0800

    refactor: 💡 thanks kevin the best man

--- day01/alpha/main.js
+++ day01/alpha/main.js
@@ -1,26 +1,42 @@
 import "./style.css";
 import "./src/header";
 
-/**
- * 當按下鍵盤上關鍵字時會發出對應的音效以及帶有css效果
- */
+// thanks fei fei
+const getKeycode = (e) => {
+  return e.key.toUpperCase().charCodeAt();
+};
 
-//keydown better than keypress because keydown keycode is same between uppercase and lowercase
-document.addEventListener("keydown", function (e) {
-  const currentAudio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+const play = (currentAudio) => {
   //if keydown is not in keys then return
   if (!currentAudio) return;
   //initial audio time;
   currentAudio.currentTime = 0;
   //play() is audio tag's function;
   currentAudio.play();
+};
 
-  //same about css style event
-  const currentKey = document.querySelector(`div[data-key="${e.keyCode}"]`);
+const toggleClass = (currentKey, isActive = false) => {
   if (!currentKey) return;
-  currentKey.classList.add("play");
-  //time out let the active effects off
-  setTimeout(() => {
-    currentKey.classList.remove("play");
-  }, 100);
-});
+  currentKey.classList.toggle("play", isActive);
+};
+
+const keyDown = (e) => {
+  const currentAudio = document.querySelector(
+    `audio[data-key="${getKeycode(e)}"]`
+  );
+  const currentKey = document.querySelector(`div[data-key="${getKeycode(e)}"]`);
+  play(currentAudio);
+  toggleClass(currentKey, true);
+};
+
+const keyUp = (e) => {
+  const currentKey = document.querySelector(`div[data-key="${getKeycode(e)}"]`);
+  toggleClass(currentKey, false);
+};
+/**
+ * 當按下鍵盤上關鍵字時會發出對應的音效以及帶有css效果
+ */
+
+//keydown better than keypress because keydown keycode is same between uppercase and lowercase
+document.addEventListener("keydown", keyDown);
+document.addEventListener("keyup", keyUp);
