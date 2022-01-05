a3102b0c25de99054571ae2cd8fdfbd10aeb1f24
KimberlyYen <87320011+KimberlyYen@users.noreply.github.com>
2022-01-05 14:11:10 +0800

    feat: 🎸 add day01 kim
    first commit
    
    BREAKING CHANGE: 🧨 no

--- day01/kim/index.js
+++ day01/kim/index.js
@@ -1,13 +1,31 @@
-// When keydown , the keycode = audio data-key, call the sound and change the class style
-window.addEventListener("keydown", function (e) {
-  // call keycode
-  const keyCurrent = e.keyCode;
-  console.log(keyCurrent);
+// Day01: When keydown , play music and change style.
+window.addEventListener("keydown", function (event) {
+  // Which keycode
+  const keyCurrent = event.keyCode;
 
-  // call class
-  const audioClass = document.querySelector(`audio[data-key='${keyCurrent}']`);
-  console.log(audioClass);
+  (function playAudio() {
+    // Keycode of audio tag
+    const audioClass = document.querySelector(
+      `audio[data-key='${keyCurrent}']`
+    );
+    if (!audioClass) return;
 
-  // if keycode === calss
-  // console.log("Right")
+    // keycode = audio, play music
+    audioClass.play();
+  })();
+
+  (function transitionStyle() {
+    // Keycode of div
+    const audioDivClass = document.querySelector(
+      `div[data-key='${keyCurrent}']`
+    );
+
+    // add
+    audioDivClass.classList.add("playing");
+
+    // remove at transitionend
+    audioDivClass.addEventListener("transitionend", function () {
+      audioDivClass.classList.remove("playing");
+    });
+  })();
 });
