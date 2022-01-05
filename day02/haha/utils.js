d2f25a32b29e963d6ed8e68d7af6b78694c9bbd6
haha861924 <qq861924@gmail.com>
2022-01-05 18:05:18 +0800

    fix: 🐛 hour hand angle

--- day02/haha/utils.js
+++ day02/haha/utils.js
@@ -14,7 +14,7 @@ function getTime() {
     const sec = new Date().getSeconds()
     // 計算時針、分針、秒針角度，時鐘初始位置為 -90 度，所以要正規化時鐘位置
     const deg = {
-        hour: 360 / 12 * hour + 90,
+        hour: 360 / 12 * hour + 90 + 360 / 12 / 60 * min, 
         min: 360 / 60 * min + 90,
         sec: 360 / 60 * sec + 90
     }
