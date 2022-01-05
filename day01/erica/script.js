0ba8c398357ec9c3341649fd91b0a6bf6a71316a
Erica <erica.du0916@gmail.com>
2022-01-06 01:18:25 +0800

    update day01 Erica answer & add readme

--- day01/erica/script.js
+++ day01/erica/script.js
@@ -2,22 +2,39 @@ function playAudio(e) {
 
     /** 先用 ASCII 排除標點符號 */
     const ascii = e.key.charCodeAt(0)
-    if(ascii < 97 || ascii > 122) return
+    if (ascii < 97 || ascii > 122) return
 
     /** 取出要控制的元件 */
     const audio = document.querySelector(`audio[data-key='${e.key}']`)
     const key = document.querySelector(`.key[data-key='${e.key}']`)
-    if(!audio) return
+    if (!audio) return
 
-    /** 加上樣式，播放聲音 */
+    /** 加上樣式，音訊回到起始時間 */
     key.classList.add('playing')
     audio.currentTime = 0
-    audio.play()
+
+    /** 處理音訊播放 */
+    const audioPromise = audio.play()
+    if (audioPromise !== undefined) {
+        audioPromise.then(() => {
+            audio.play()
+        })
+        .catch(() => {
+            stopAudio()
+        })
+    }
 }
 
 function stopAudio() {
     const keys = document.querySelectorAll('.key')
+    const audios = document.querySelectorAll('audio')
+    
+    /** 移除樣式、停止音訊 */
     keys.forEach((key) => key.classList.remove('playing'))
+    audios.forEach((audio) => {
+        audio.pause()
+        audio.currentTime = 0
+    })
 }
 
 document.addEventListener('keydown', playAudio)
