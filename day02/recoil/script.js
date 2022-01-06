a8022feae3bddd59aaa70c3e20ec08aaaa7c0c0f
Peter Yeh <leev126g8@gmail.com>
2022-01-06 12:56:35 +0800

    fix: 🐛 modify hour tick formular

--- day02/recoil/script.js
+++ day02/recoil/script.js
@@ -5,13 +5,13 @@ const degree = (time = 0) => `transform: rotateZ(${time + 90}deg);`;
 const hourElement = $(".hour-hand");
 const minuteElement = $(".min-hand");
 const secondElement = $(".second-hand");
-const secondDeg = 360 / (30 * 2);
-const minuteDeg = 360 / 30;
-const hourDegree = 360 / 12;
+const secondDeg = 360 / 60;
+const minuteDeg = 360 / 60;
+const hourDegree = 30 / 60;
 
 setInterval(() => {
   const now = new Date();
   secondElement.style = degree(now.getSeconds() * secondDeg);
   minuteElement.style = degree(now.getMinutes() * minuteDeg);
-  hourElement.style = degree(now.getHours() * hourDegree);
+  hourElement.style = degree((now.getHours() * hourDegree)+(now.getMinutes() * hourDegree));
 }, 1000);
