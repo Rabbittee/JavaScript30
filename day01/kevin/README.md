f02dd7b30005a420e8a425be7edec102c05b0962
kevin_hws <kevinshu1995@gmail.com>
2022-01-06 23:08:36 +0800

    fix: 🐛 移除 IIFE

--- day01/kevin/README.md
+++ day01/kevin/README.md
@@ -5,7 +5,6 @@
 - 監聽 `keydown` & `keyup`
 - 用 [Set](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Set) 紀錄正被按著的按鈕 (keycode)，因為要用來避免按著按紐時重複撥放音訊 (監聽的 `keydown` 在按鈕壓著時會重複執行)
 - audio play 之前先把 `currentTime` 設定為 0，不然下次播放會從上次停止撥放的地方播放
-- 使用 IIFE，為甚麼呢? 這樣才不會讓變數汙染其他地方，[不信的話點進來開 F12 來看看](https://rabbittee.github.io/JavaScript30/day01/kevin/)
 
 ## 流程
 
--- day01/kevin/index.js
+++ day01/kevin/index.js
@@ -1,52 +1,48 @@
-(function keyBoardDrum() {
-  document.addEventListener("keydown", keydownHandler);
-  document.addEventListener("keyup", keyupHandler);
+document.addEventListener("keydown", keydownHandler);
+document.addEventListener("keyup", keyupHandler);
 
-  // * 紀錄那些正在被觸發的 keycode
-  const activeKeys = new Set();
+// * 紀錄那些正在被觸發的 keycode
+const activeKeys = new Set();
 
-  // * 按下去的時候
-  function keydownHandler(event) {
-    const keycode = getKeycode(event);
-    if (activeKeys.has(keycode)) return;
+// * 按下去的時候
+function keydownHandler(event) {
+  const keycode = getKeycode(event);
+  if (activeKeys.has(keycode)) return;
 
-    playAudio(keycode);
-    toggleActiveBoxClass(keycode, true);
-    activeKeys.add(keycode);
-  }
+  playAudio(keycode);
+  toggleActiveBoxClass(keycode, true);
+  activeKeys.add(keycode);
+}
 
-  // * 放開的時候
-  function keyupHandler(event) {
-    const keycode = getKeycode(event);
-    activeKeys.delete(keycode);
-    toggleActiveBoxClass(keycode, false);
-  }
+// * 放開的時候
+function keyupHandler(event) {
+  const keycode = getKeycode(event);
+  activeKeys.delete(keycode);
+  toggleActiveBoxClass(keycode, false);
+}
 
-  function getKeycode(event) {
-    return event.key.toUpperCase().charCodeAt();
-  }
+function getKeycode(event) {
+  return event.key.toUpperCase().charCodeAt();
+}
 
-  // * Audio relates functions =======
-  function getTargetAudio(keycode) {
-    return document.querySelector(`audio[data-key="${keycode}"]`);
-  }
+// * Audio relates functions =======
+function getTargetAudio(keycode) {
+  return document.querySelector(`audio[data-key="${keycode}"]`);
+}
 
-  function playAudio(keycode) {
-    const targetAudio = getTargetAudio(keycode);
+function playAudio(keycode) {
+  const targetAudio = getTargetAudio(keycode);
 
-    if (targetAudio === null) return;
-    targetAudio.currentTime = 0;
-    targetAudio.play();
-  }
+  if (targetAudio === null) return;
+  targetAudio.currentTime = 0;
+  targetAudio.play();
+}
 
-  // * View relates functions ======
-  function toggleActiveBoxClass(keycode, isActive = false) {
-    const activeClassName = "playing";
-    const targetBox = document.querySelector(`.key[data-key="${keycode}"]`);
+// * View relates functions ======
+function toggleActiveBoxClass(keycode, isActive = false) {
+  const activeClassName = "playing";
+  const targetBox = document.querySelector(`.key[data-key="${keycode}"]`);
 
-    if (targetBox === null) return;
-    targetBox.classList.toggle(activeClassName, isActive);
-  }
-})();
-
-const whyIIFE = "就說要用IIFE吧";
+  if (targetBox === null) return;
+  targetBox.classList.toggle(activeClassName, isActive);
+}
