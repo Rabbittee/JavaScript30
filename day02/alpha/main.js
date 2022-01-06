7471e0da23248ecca5bfdd6349ed9c092a6354a4
Alpha <46735230+alphatero@users.noreply.github.com>
2022-01-06 13:50:47 +0800

    feat: 🎸  day 2 alpha's solution (#41)
    * feat: 🎸 day one beginner
    
    * feat: 🎸 add readme.md and build html first time
    
    * feat: 🎸 add header component
    
    * fix: 🐛 add dist
    
    * fix: 🐛 sample assets link
    
    * fix: 🐛 assets link fixed
    
    * fix: 🐛 readme content
    
    * feat: 🎸 first finsih before refactor
    
    * refactor: 💡 day2 and add readme
    
    * refactor: 💡 refresh header.js
    
    * fix: 🐛 get hours deg error
    
    * fix: 🐛 remove day1
    
    * fix: 🐛 clock caculate function
    
    * fix: 🐛 some issue
    
    * fix: 🐛 build dist

---
+++ day02/alpha/main.js
@@ -0,0 +1,62 @@
+import "./style.css";
+import "./src/header";
+
+/**
+ * 利用js來讓指針動的跟真的時鐘一樣
+ */
+
+//select DOM
+const select = (item) => document.querySelector(item);
+
+//caculate degree
+const caculateDeg = (time, denominator) => {
+  return (time / denominator) * 360;
+};
+
+//add rotate style of clock's hand
+const rotate = (element, getDeg) => {
+  element.style.transform = `rotate(${getDeg + 90}deg)`;
+};
+
+//get whole clock action
+const getClock = () => {
+  //get now time
+  const nowTime = new Date();
+
+  //create clock hand's object about element and time value
+  const hourHand = { element: select(".hour-hand"), value: nowTime.getHours() };
+
+  const minHand = { element: select(".min-hand"), value: nowTime.getMinutes() };
+
+  const secondHand = {
+    element: select(".second-hand"),
+    value: nowTime.getSeconds(),
+  };
+
+  //add about string to format time value
+  hourHand.string = hourHand.value < 10 ? "0" + hourHand.value : hourHand.value;
+  minHand.string = minHand.value < 10 ? "0" + minHand.value : minHand.value;
+  secondHand.string =
+    secondHand.value < 10 ? "0" + secondHand.value : secondHand.value;
+
+  //view time string
+  const timeView = select(".time-value");
+  timeView.innerHTML = `<span>${hourHand.string}:${minHand.string}:${secondHand.string}</span>`;
+
+  //do rotate about clock hands
+  rotate(
+    hourHand.element,
+    caculateDeg(
+      hourHand.value + minHand.value / 60 + secondHand.value / 3600,
+      12
+    )
+  );
+  rotate(
+    minHand.element,
+    caculateDeg(minHand.value + secondHand.value / 60, 60)
+  );
+  rotate(secondHand.element, caculateDeg(secondHand.value, 60));
+};
+
+//do again pre second
+setInterval(getClock, 1000);
