171394ef114fa3f53bae781720dc6558bc88ffa3
haha861924 <qq861924@gmail.com>
2022-01-06 13:32:44 +0800

    refactor: 💡 day02 answer

--- day02/haha/utils.js
+++ day02/haha/utils.js
@@ -1,27 +1,43 @@
 const hourTarget = document.querySelector('.hour-hand');
 const minTarget = document.querySelector('.min-hand');
 const secTarget = document.querySelector('.second-hand');
-
+const regularHand = 90;
 // 調整時針、分針、秒針的指針位置
-hourTarget.style.transformOrigin = 'right';
-minTarget.style.transformOrigin = 'right';
-secTarget.style.transformOrigin = 'right';
+const handStyle = (hand) => (hand.style.transformOrigin = 'right');
 
-function getTime() {
+const getTime = () => {
   // 獲取時針、分針、秒針時間
-  const hour = new Date().getHours() % 12; // 12 小時制
-  const min = new Date().getMinutes();
-  const sec = new Date().getSeconds();
+  const date = new Date();
+  const hour = date.getHours() % 12;
+  const min = date.getMinutes();
+  const sec = date.getSeconds();
+  return { hour, min, sec };
+};
+
+const handAngle = (grid, time) => (360 / grid) * time;
+
+const getAngle = (hour, min, sec) => {
   // 計算時針、分針、秒針角度，時鐘初始位置為 -90 度，所以要正規化時鐘位置
-  const deg = {
-    hour: (360 / 12) * hour + 90 + (360 / 12 / 60) * min,
-    min: (360 / 60) * min + 90,
-    sec: (360 / 60) * sec + 90,
-  };
+  const hourAngle = handAngle(12, hour) + handAngle(12, min) / 60 + regularHand;
+  const minAngle = handAngle(60, min) + regularHand;
+  const secAngle = handAngle(60, sec) + regularHand;
+  return { hourAngle, minAngle, secAngle };
+};
+
+const clockStyle = (hour, min, sec) => {
   // css style
-  hourTarget.style.transform = `rotate(${deg.hour}deg)`;
-  minTarget.style.transform = `rotate(${deg.min}deg)`;
-  secTarget.style.transform = `rotate(${deg.sec}deg)`;
+  hourTarget.style.transform = `rotate(${hour}deg)`;
+  minTarget.style.transform = `rotate(${min}deg)`;
+  secTarget.style.transform = `rotate(${sec}deg)`;
+};
+
+function handleEvent() {
+  handStyle(hourTarget);
+  handStyle(minTarget);
+  handStyle(secTarget);
+  const { hour, min, sec } = getTime();
+  const { hourAngle, minAngle, secAngle } = getAngle(hour, min, sec);
+  clockStyle(hourAngle, minAngle, secAngle);
 }
 
-setInterval(getTime, 1000);
+setInterval(handleEvent, 1000);
