2b3e484b18960426d2236081fa7ad993ffe03c47
KimberlyYen <87320011+KimberlyYen@users.noreply.github.com>
2022-01-06 15:46:19 +0800

    feat: 🎸 [Pedding] Clock can move by second
    Clock can move but time need fix.
    
    BREAKING CHANGE: 🧨 no

--- day02/kim/index.js
+++ day02/kim/index.js
@@ -1,3 +1,52 @@
-setTimeout(function () {
-  console.log("hi");
-}, 1000 * i);
+/**
+ * 每秒鐘改變時鐘樣式
+ */
+
+/**
+ * 每秒鐘執行
+ */
+function everySecondChange() {
+  changeStyle();
+  // nowTime();
+  setTimeout(everySecondChange, 1000);
+}
+setTimeout(everySecondChange, 1000);
+
+function changeStyle() {
+  /**
+   * 呼叫時鐘
+   */
+  // hour
+  // min
+  // second
+  const date = new Date();
+  const dateSecond = date.getSeconds();
+
+  /**
+   * 呼叫樣式
+   * 變更樣式
+   */
+  const hour = document.querySelector(".hour-hand");
+  // console.log((hour.style.transform = `rotate(${dateSecond}deg)`));
+  // hour.style.transform = "rotate(20deg)";
+  hour.style.transform = `rotate(${dateSecond}deg)`;
+
+  const min = document.querySelector(".min-hand");
+  const second = document.querySelector(".second-hand");
+}
+
+// function nowTime() {
+//   /**
+//    * 呼叫時鐘
+//    */
+//   // hour
+//   // min
+//   // second
+//   const date = new Date();
+//   const dateSecond = date.getSeconds();
+//   console.log(dateSecond);
+// }
+
+/**
+ * 對應起來修改
+ */
