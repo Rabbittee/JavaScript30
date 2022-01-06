302f4fb12802cfb14084de17f05505e052055701
HuskyHsu <schi.hsu@gmail.com>
2022-01-06 22:30:10 +0800

    feat: 🎸 day01 done

--- day01/Husky/src/components/KeyButton.tsx
+++ day01/Husky/src/components/KeyButton.tsx
@@ -5,11 +5,9 @@ export default function KeyButton(props: Key) {
     <div
       className={
         "border-4 border-solid border-black rounded-lg m-4 text-2xl py-4 px-2 w-40 " +
-        "text-center text-white bg-black bg-opacity-40 cursor-pointer " +
-        "transition-all " +
+        "text-center text-white bg-black bg-opacity-40 cursor-pointer transition-all " +
         (props.playing ? "playing" : "")
       }
-      onClick={props.onClick}
     >
       <kbd className="block text-6xl">{props.letter}</kbd>
       <span className="text-xl uppercase tracking-widest text-yellow-500">
