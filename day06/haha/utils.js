9137bf24b58913bccd077ee7264c9991f02d45a7
haha861924 <qq861924@gmail.com>
2022-01-06 18:22:04 +0800

    feat: 🎸 when focus input delete placeholder, leave recover

--- day06/haha/utils.js
+++ day06/haha/utils.js
@@ -1,1 +1,16 @@
-const endpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
+const endpoint =
+  "https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json";
+
+const input = document.querySelector("input");
+
+input.addEventListener("focus", clearContent);
+input.addEventListener("blur", testInputVal);
+
+function clearContent() {
+  this.setAttribute("placeholder", "");
+}
+
+function testInputVal() {
+  const text = this.value === "" ? "City or State" : this.value;
+  this.setAttribute("placeholder", text);
+}
