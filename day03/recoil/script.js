e61b1423fe23fcf8cce16c4a4f712635ec0a539a
Peter Yeh <leev126g8@gmail.com>
2022-01-07 17:05:14 +0800

    refactor: 💡 modify code

--- day03/recoil/script.js
+++ day03/recoil/script.js
@@ -7,29 +7,22 @@ function handleValue(target) {
   return target.value + sizing;
 }
 
-function handleUpdate({ target }) {
-  root.style.setProperty(`--${target.name}`, handleValue(target));
-}
 
-function init(nodes) {
-  nodes.forEach(function (target) {
-    root.style.setProperty(`--${target.name}`, handleValue(target));
+function watchInputEffect(rootNode, node) {
+  node.addEventListener("change", ({ target })=>{
+    rootNode.style.setProperty(`--${target.name}`, handleValue(target));
   });
-  return nodes;
+  node.addEventListener("mousedown", ({ target })=>{
+    rootNode.style.setProperty(`--${target.name}`, handleValue(target));
+  });
 }
 
-function watch(nodes) {
+function handleInputEffect(documentEl, nodes) {
   nodes.forEach(function (node) {
-    node.addEventListener("change", handleUpdate);
-    node.addEventListener("mousemove", handleUpdate);
+    documentEl.style.setProperty(`--${node.name}`, handleValue(node));
+    watchInputEffect(documentEl,node);
   });
-  return nodes;
 }
 
 const inputs = $(".controls > input");
-
-function run() {
-  Promise.resolve(inputs).then(init).then(watch);
-}
-
-run();
+handleInputEffect(root, inputs);
