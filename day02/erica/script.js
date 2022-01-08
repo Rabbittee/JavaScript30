b41d2c9d170acc1bc602f612cb480a9b6e794934
Erica <erica.du0916@gmail.com>
2022-01-08 21:24:40 +0800

    update: add readme & script comment

--- day02/erica/script.js
+++ day02/erica/script.js
@@ -12,17 +12,19 @@ function setClockTime() {
     const scale = 360 / 12 / 5
     const translate = `translate(-50%, -100%)`
 
+    // 時針：(時 * 刻度 * 5(小格)) + (分 / 60(小格) * 刻度)
     const hourDeg = (hour * scale * 5) + Math.floor(min / 60 * scale)
     hourHand.style.transform = `${translate} rotate(${hourDeg}deg)`
 
+    // 分針：(分 * 刻度) + (秒 / 60(小格) * 刻度)
     const minDeg = (min * scale) + Math.floor(sec / 60 * scale)
     minHand.style.transform = `${translate} rotate(${minDeg}deg)`
 
-    let secDeg = sec * scale
+    // 秒針：秒 * 刻度
+    const secDeg = sec * scale
     secHand.style.transform = `${translate} rotate(${secDeg}deg)`
 
 }
-
 
 setInterval(setClockTime, 1000);
 setClockTime();
