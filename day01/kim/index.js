e71ab9c41e4cbd214313306c72fd719b2a219902
Kimberly <yantingfen@yantingfendeMacBook-Air.local>
2022-01-08 23:56:25 +0800

    fix: 🐛 fixed day01 kim
    - Remove IIFE

--- day01/kim/index.js
+++ day01/kim/index.js
@@ -1,31 +1,32 @@
-// Day01: When keydown , play music and change style.
+// 視窗監聽，鍵盤點下事件
 window.addEventListener("keydown", function (event) {
-  // Which keycode
+  // 鍵盤點到哪一顆按鍵？ 按鍵號碼？
   const keyCurrent = event.keyCode;
 
-  (function playAudio() {
-    // Keycode of audio tag
-    const audioClass = document.querySelector(
-      `audio[data-key='${keyCurrent}']`
-    );
+  // 何時播放音樂
+  function playAudio() {
+    // 按鍵號碼
+    const audioClass = document.querySelector(`audio[data-key='${keyCurrent}']`);
     if (!audioClass) return;
 
-    // keycode = audio, play music
+    // 按鍵號碼 = 音樂號碼，就播音樂
+    // 每次點擊之間，要取消上一次
+    audioClass.currentTime = 0;
     audioClass.play();
-  })();
+  }
 
-  (function transitionStyle() {
-    // Keycode of div
-    const audioDivClass = document.querySelector(
-      `div[data-key='${keyCurrent}']`
-    );
+  // 何時換樣式
+  function transitionStyle() {
+    // 樣式的 DOM
+    const audioDivClass = document.querySelector(`div[data-key='${keyCurrent}']`);
 
-    // add
+    // 增加樣式
     audioDivClass.classList.add("playing");
-
-    // remove at transitionend
+    // 動畫的最後，移除樣式
     audioDivClass.addEventListener("transitionend", function () {
       audioDivClass.classList.remove("playing");
     });
-  })();
+  }
+  playAudio();
+  transitionStyle();
 });
