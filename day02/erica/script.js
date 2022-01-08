31af7647549e554b4bc556ef7f444105c0192d8d
Erica <erica.du0916@gmail.com>
2022-01-08 22:21:18 +0800

    refactor: Erica day02

--- day02/erica/script.js
+++ day02/erica/script.js
@@ -3,28 +3,34 @@ const hourHand = getDom('.hour-hand')
 const minHand = getDom('.min-hand')
 const secHand = getDom('.second-hand')
 
+/**
+ * 時針：(時 * 刻度 * 5(小格)) + (分 / 60(小格) * 刻度)
+ * 分針：(分 * 刻度) + (秒 / 60(小格) * 刻度)
+ * 秒針：秒 * 刻度
+ */
 function setClockTime() {
     const now = new Date()
     const hour = Math.floor(now.getHours() % 12)
     const min = now.getMinutes()
     const sec = now.getSeconds()
 
-    const scale = 360 / 12 / 5
-    const translate = `translate(-50%, -100%)`
-
-    // 時針：(時 * 刻度 * 5(小格)) + (分 / 60(小格) * 刻度)
-    const hourDeg = (hour * scale * 5) + Math.floor(min / 60 * scale)
-    hourHand.style.transform = `${translate} rotate(${hourDeg}deg)`
-
-    // 分針：(分 * 刻度) + (秒 / 60(小格) * 刻度)
-    const minDeg = (min * scale) + Math.floor(sec / 60 * scale)
-    minHand.style.transform = `${translate} rotate(${minDeg}deg)`
-
-    // 秒針：秒 * 刻度
-    const secDeg = sec * scale
-    secHand.style.transform = `${translate} rotate(${secDeg}deg)`
+    changeTranform(hourHand, getRotateDeg(hour * 5, min))
+    changeTranform(minHand, getRotateDeg(min, sec))
+    changeTranform(secHand, getRotateDeg(sec))
 
 }
 
-setInterval(setClockTime, 1000);
-setClockTime();
+function getRotateDeg(curr, next) {
+    const unit = 360 / 12 / 5
+    if(next) return (curr * unit) + Math.floor(next / 60 * unit)
+    else return curr * unit
+}
+
+function changeTranform(target, degrees) {
+    const translate = `translate(-50%, -100%)`
+    return target.style.transform = `${translate} rotate(${degrees}deg)`
+}
+
+setClockTime()
+setInterval(setClockTime, 1000)
+
