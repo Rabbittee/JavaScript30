4ccf8042bea91925dff51403ca7a83e5a8d4198b
Peter Yeh <leev126g8@gmail.com>
2022-01-11 07:39:01 +0800

    feat: 🎸 add day03 Recoil
    抱歉 push時被要求pull不小心合併到分支上，在此奉上十份奶捲

---
+++ day03/recoil/script.js
@@ -0,0 +1,32 @@
+const root = document.documentElement;
+const setRootStyle = style(root);
+const $ = (target) => document.querySelectorAll(target);
+
+function style(node){
+  return function(propName,val){
+    node.style.setProperty(propName, val);
+  }
+}
+
+function watchInputEffect(node) {
+  node.addEventListener("change", ({ target })=>{
+    const sizing = target.dataset?.sizing ?? "";
+    setRootStyle(`--${target.name}`,target.value + sizing)
+  });
+
+  node.addEventListener("mousemove", ({ target })=>{
+    const sizing = target.dataset?.sizing ?? "";
+    setRootStyle(`--${target.name}`, target.value + sizing)
+  });
+}
+
+function handleInputEffect(nodes) {
+  nodes.forEach(function (node) {
+    const sizing = node.dataset?.sizing ?? "";
+    setRootStyle(`--${node.name}`, node.value + sizing);//init root style
+    watchInputEffect(node);
+  });
+}
+
+const inputs = $(".controls > input");
+handleInputEffect(inputs);
