edf141a01565e719ca9eed0459e1dc0fb102548f
haha861924 <qq861924@gmail.com>
2022-01-10 16:58:45 +0800

    fix: 🐛 restart audio play time

--- day01/haha/utils.js
+++ day01/haha/utils.js
@@ -1,10 +1,11 @@
 document.body.addEventListener("keydown", getKey, false);
 
 function getKey(e) {
-  const char = e.key.toUpperCase().charCodeAt()
+  const char = e.key.toUpperCase().charCodeAt();
   const audio = document.querySelector(`audio[data-key="${char}"]`);
   const target = document.querySelector(`div[data-key="${char}"]`);
   if (!audio) return false;
+  audio.currentTime = 0;
   audio.play();
   target.classList.add("playing");
   target.addEventListener("transitionend", removeClass);
