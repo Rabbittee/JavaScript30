77c0e115c4528290f94fca293cccf6cb993adcdc
Peter Yeh <leev126g8@gmail.com>
2022-01-10 10:34:15 +0800

    refactor: 💡 remove unesed method & use destructuring

--- day05/recoil/script.js
+++ day05/recoil/script.js
@@ -1,27 +1,15 @@
 const $ = (target) => document.querySelectorAll(target);
 
-function Elements(list) {
-  return function (className) {
-    list.forEach(function (node) {
-      node.classList.remove(className);
-    });
-  };
-}
-
 const panels = $(".panel");
-const removeAllPanelClass = Elements(panels);
 
 panels.forEach(function (element) {
-  element.addEventListener("click", function (e) {
-    if (e.target.classList.contains("open")) {
-      e.target.classList.remove("open");
-    } else {
-      removeAllPanelClass("open");
-      e.target.classList.add("open");
-    }
+  element.addEventListener('click', function ({ target: { classList } }) {
+    classList.toggle('open');
   });
-  element.addEventListener("transitionend",function(e){
-    if (e.target.classList.contains("open"))e.target.classList.add("open-active");
-    else e.target.classList.remove("open-active");
-  })
+  element.addEventListener(
+    'transitionend',
+    function ({ target: { classList } }) {
+      classList.contains("open") ? classList.add("open-active") : classList.remove("open-active");
+    }
+  );
 });
