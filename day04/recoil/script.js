2ac68fd719301855a165e6640d852fd0891fc519
Peter Yeh <leev126g8@gmail.com>
2022-01-10 09:04:08 +0800

    refactor: 💡 semantic variant

--- day04/recoil/script.js
+++ day04/recoil/script.js
@@ -64,14 +64,16 @@ const people = [
 
 // Array.prototype.filter()
 // 1. Filter the list of inventors for those who were born in the 1500's
+const startTime = 1500; 
+const s = 100;
 const borns = inventors.filter(
-  (inventor) => inventor.year >= 1500 && inventor.year < 1600
+  (inventor) => inventor.year >= startTime && inventor.year < startTime + s
 );
 console.table(borns);
 
 // Array.prototype.map()
 // 2. Give us an array of the inventors first and last names
-const names = inventors.map((inventor) => `${inventor.first} ${inventor.last}`);
+const names = inventors.map(({first, last}) => `${first} ${last}`);
 console.log("inventors first and last names", names);
 // Array.prototype.sort()
 // 3. Sort the inventors by birthdate, oldest to youngest
@@ -95,7 +97,7 @@ console.table("years lived", livedList);
 // 7. sort Exercise
 // Sort the people alphabetically by last name
 function getLastName(name) {
-  const lastName = name.split(", ")[0];
+  const [lastName] = name.split(", ");
   return lastName.toUpperCase();
 }
 const sortAlphabet = people.sort((a, b) => {
