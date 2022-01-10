8b2363a8bf122aea30de8f917f6b10fac58cbe33
Peter Yeh <leev126g8@gmail.com>
2022-01-10 15:48:12 +0800

    refactor: 💡 day06 Recoil

--- day06/recoil/script.js
+++ day06/recoil/script.js
@@ -1,44 +1,68 @@
-const url = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
-const debounce = (delay,func) =>{
-    let timer
-    return (payload) => {
-        if(timer) clearTimeout(timer)
-        timer = setTimeout(()=>func(payload),delay)
-    }
-}
+const url =
+  "https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json";
+
 const select = (target) => document.querySelector(target);
-const searchBar = select("input.search")
+
+//reduce trigger frequency
+const debounce = (delay = 0, func) => {
+  let timer;
+  return (payload) => {
+    if (timer) clearTimeout(timer);
+    timer = setTimeout(() => func(payload), delay);
+  };
+};
+
+// highlight keyword by searching
+function findKeyword(keyword) {
+  const regex = new RegExp(`(${keyword})`, "gi");
+  return function (content) {
+    return content.replace(regex, "<span class='hl'>$1</span>");
+  };
+}
+
+//handle side effect of api
+async function apiEffect(source) {
+  const payload = await fetch(source);
+  return await payload.json();
+}
+
+const filterTourCity = (keyword) => (tours) => {
+  const regex = new RegExp(keyword, "gi");
+  return tours.filter((tour) => {
+    return tour.city.match(regex) || tour.population.match(regex);
+  });
+};
+
+const highlightKeyword = (keyword) => (tours) => {
+  const focusKeyword = findKeyword(keyword);
+  return tours.map((tour) => {
+    return `
+            <li>
+                <span class="name">
+                ${focusKeyword(tour.city)}
+                </span>
+                <span class="population">
+                ${focusKeyword(tour.population)}
+                </span>
+            </li>
+        `;
+  });
+};
+
+const combineItem = (tours) => tours.join("");
+
+const insertTo = (node) => (list) => (node.innerHTML = list);
+
+const typeSearch = select("input.search");
+
 const list = select("ul.suggestions");
 
-function highLight(keyword,content){
-    const regex = new RegExp(`(${keyword})`,"gi");
-    return content.replace(regex,"<span class='hl'>$1</span>")
-}
-
-async function handleSearch(keyword){
-    try{
-        const payload = await fetch(url);
-        const tourSeries = await payload.json();
-        if(tourSeries && Array.isArray(tourSeries)){
-            const result = tourSeries.filter(
-                tour=>tour.city.includes(keyword)
-            )
-            .map(
-                tour=>{
-                    return `
-                        <li>
-                            <span class="name">${highLight(keyword,tour.city)}</span>
-                            <span class="population">${tour.population}</span>
-                        </li>
-                    `
-                }
-            ).join("")
-            list.innerHTML = result;
-        }
-    }catch(err){
-        console.warn(err)
-    }
-}
-searchBar.addEventListener("change",function({target:{value}}){
-    debounce(600,handleSearch)(value)
-})
+typeSearch.addEventListener("keyup", function ({ target: { value } }) {
+  debounce(1000, function (queryContent) {
+    apiEffect(url)
+      .then(filterTourCity(queryContent))
+      .then(highlightKeyword(queryContent))
+      .then(combineItem)
+      .then(insertTo(list));
+  })(value);
+});
