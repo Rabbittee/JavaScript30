8b0d091b050d8e8827953ce8fb7821bda27cdb67
ShihChi, Hsu <schi.hsu@gmail.com>
2022-01-12 00:10:27 +0800

    [feature] add day01 husky (#55)
    * feat: 🎸 day01 init
    
    * feat: 🎸 update day01 answer
    
    * feat: 🎸 day01 add addEventListener event
    
    * feat: 🎸 day01 done
    
    * feat: 🎸 day01 init
    
    * feat: 🎸 update day01 answer
    
    * feat: 🎸 day01 add addEventListener event
    
    * feat: 🎸 day01 done
    
    * fix: 🐛 fix pull request comment of react suggest

---
+++ day01/Husky/src/assets/sound.ts
@@ -0,0 +1,23 @@
+import boom from "@/sounds/boom.wav";
+import clap from "@/sounds/clap.wav";
+import hihat from "@/sounds/hihat.wav";
+import kick from "@/sounds/kick.wav";
+import openhat from "@/sounds/openhat.wav";
+import ride from "@/sounds/ride.wav";
+import snare from "@/sounds/snare.wav";
+import tink from "@/sounds/tink.wav";
+import tom from "@/sounds/tom.wav";
+
+const soundList = [
+  { name: "clap", letter: "A", playing: false, sound: clap },
+  { name: "hihat", letter: "S", playing: false, sound: hihat },
+  { name: "kick", letter: "D", playing: false, sound: kick },
+  { name: "openhat", letter: "F", playing: false, sound: openhat },
+  { name: "boom", letter: "G", playing: false, sound: boom },
+  { name: "ride", letter: "H", playing: false, sound: ride },
+  { name: "snare", letter: "J", playing: false, sound: snare },
+  { name: "tom", letter: "K", playing: false, sound: tom },
+  { name: "tink", letter: "L", playing: false, sound: tink },
+];
+
+export default soundList;
