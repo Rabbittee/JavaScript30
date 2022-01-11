d6e73e6ee3fa8609c19079eb5634e0d5ea86cde5
ShihChi, Hsu <schi.hsu@gmail.com>
2022-01-12 00:10:27 +0800

    [feature] add day01 husky (#55)
    * feat: 🎸 day01 init
    
    * feat: 🎸 update day01 answer
    
    * feat: 🎸 day01 add addEventListener event
    
    * feat: 🎸 day01 done
    
    * feat: 🎸 day01 init
    
    * feat: 🎸 update day01 answer
    
    * feat: 🎸 day01 add addEventListener event
    
    * feat: 🎸 day01 done
    
    * fix: 🐛 fix pull request comment of react suggest

---
+++ day01/Husky/src/components/KeyButton.tsx
@@ -0,0 +1,20 @@
+import Key from "../models/Key";
+import clsx from "clsx";
+
+export default function KeyButton(props: Key) {
+  return (
+    <div
+      className={clsx([
+        "border-4 border-solid border-black rounded-lg bg-black bg-opacity-40",
+        "m-4 py-4 px-2 w-40 text-2xl",
+        "text-center text-white  transition-all",
+        { playing: props.playing },
+      ])}
+    >
+      <kbd className="block text-6xl">{props.letter}</kbd>
+      <span className="text-xl uppercase tracking-widest text-yellow-500">
+        {props.name}
+      </span>
+    </div>
+  );
+}
---
+++ day01/Husky/src/utils/music.ts
@@ -0,0 +1,9 @@
+import Key from "../models/Key";
+
+function play(sound: Key) {
+  const audio = new Audio(sound?.sound);
+  audio.currentTime = 0;
+  audio.play();
+}
+
+export { play };
