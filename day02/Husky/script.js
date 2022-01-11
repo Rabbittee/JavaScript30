81631125661b51e28c2789420d812f60e320c29c
ShihChi, Hsu <schi.hsu@gmail.com>
2022-01-11 14:13:02 +0800

    fix: 59 to 0 css transition bug (day02) (#91)
    * feat: 🎸 add day02 husky answer
    
    * feat: 🎸 add transition timing function
    
    * fix: 🐛 59 to 0 css transition bug
    
    * fix: 🐛 object literal extension

--- day02/Husky/script.js
+++ day02/Husky/script.js
@@ -1,27 +1,43 @@
-function getTimeAngle() {
-    const now = new Date()
-    const base = 90
+const SHIFT = 90
+const CIRCLE = 360
 
-    const sec = base + now.getSeconds() * 6
-    const min = base + now.getMinutes() * 6 + now.getSeconds() / 10
-    const hour = base + (now.getHours() % 12) * 30 + now.getMinutes() / 2
+function getTimeAngle([hourCurrentCircle, minCurrentCircle, secCurrentCircle]) {
+    const now = new Date()
+
+    const sec = SHIFT + secCurrentCircle * CIRCLE + now.getSeconds() * 6
+    const min = SHIFT + minCurrentCircle * CIRCLE + now.getMinutes() * 6 + now.getSeconds() / 10
+    const hour = SHIFT + hourCurrentCircle * CIRCLE + (now.getHours() % 12) * 30 + now.getMinutes() / 2
 
     return {
         sec,
         min,
-        hour
+        hour,
+        carry: {
+            sec: now.getSeconds() === 59,
+            min: now.getMinutes() === 59 && now.getSeconds() === 59,
+            hour: (now.getHours() % 12) === 11 && now.getMinutes() === 59 && now.getSeconds() === 59
+        }
     }
 }
 
 function setHand() {
-    const angle = getTimeAngle()
     const hourHand = document.querySelector('.hour-hand')
     const minHand = document.querySelector('.min-hand')
     const secondHand = document.querySelector('.second-hand')
 
+    const angle = getTimeAngle([
+        Number(hourHand.dataset.circle),
+        Number(minHand.dataset.circle),
+        Number(secondHand.dataset.circle)
+    ])
+
     secondHand.style.transform = `rotate(${angle.sec}deg)`;
     minHand.style.transform = `rotate(${angle.min}deg)`;
     hourHand.style.transform = `rotate(${angle.hour}deg)`;
+
+    if (angle.carry.sec) secondHand.dataset.circle = Number(secondHand.dataset.circle) + 1
+    if (angle.carry.min) minHand.dataset.circle = Number(minHand.dataset.circle) + 1
+    if (angle.carry.hour) hourHand.dataset.circle = Number(hourHand.dataset.circle) + 1
 }
 
 setInterval(setHand, 1000)
