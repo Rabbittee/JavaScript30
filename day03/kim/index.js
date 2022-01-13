0216f49cdb9589f01cf1a47cceaf4dcbe4aeb121
KimberlyYen <87320011+KimberlyYen@users.noreply.github.com>
2022-01-13 11:09:17 +0800

    fix: 🐛 remove console.log, fix path of HTML
    HTML path add "." become ./style.css

--- day03/kim/index.js
+++ day03/kim/index.js
@@ -2,10 +2,9 @@
 const target = document.querySelectorAll(".controls");
 
 // 監聽事件：改變(聽三組)
-// Change method by bubbling event.
+// Method by bubbling event.
 target.forEach((element) => {
   element.addEventListener("input", function (e) {
-    console.log(e.target);
     const currentName = e.target.name;
     const currentValue = e.target.value;
     const unit = e.target.dataset.sizing || "";
