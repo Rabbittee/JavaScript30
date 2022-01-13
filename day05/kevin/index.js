d8e2aaebea5b219dde920447d2df3f2e7268bf6c
kevinhws <kevinshu1995@gmail.com>
2022-01-13 17:50:28 +0800

    feat: 🎸 add day5 solution

---
+++ day05/kevin/index.js
@@ -0,0 +1,12 @@
+document.addEventListener("click", (e) => {
+  const clickedPanel = e.target.closest(".panel");
+  const hasOpened = clickedPanel.classList.contains("open");
+
+  const transitionHandler = () => {
+    clickedPanel.classList.toggle("active", !hasOpened);
+    clickedPanel.removeEventListener("transitionend", transitionHandler);
+  };
+
+  clickedPanel.classList.toggle("open");
+  clickedPanel.addEventListener("transitionend", transitionHandler);
+});
