2974530eca4637bb26fd6a70da2d8459f5430301
haha861924 <qq861924@gmail.com>
2022-01-15 03:18:49 +0800

    refactor: 💡 panel transition end handle event

--- day05/haha/utils.js
+++ day05/haha/utils.js
@@ -3,22 +3,15 @@ const selectNode = (target) =>
   target.nodeName === "DIV" ? target : target.parentNode;
 
 panel.addEventListener("click", clickPanel, false);
+panel.addEventListener("transitionend", expandPanel, false);
 
-panels.forEach((el) => {
-  // 監聽動畫結束
-  el.addEventListener("transitionend", subtitleTransition);
-});
-
-// 展開收合 card ( toggle 自動切換)
-function clickToggle() {
-  this.classList.toggle("open");
 // 展開收合 card，添加 open class
 function clickPanel({ target }) {
   selectNode(target).classList.toggle("open");
 }
 
-// class 包不包含 open 決定 open-active 的去留
-function subtitleTransition() {
-  if (this.classList.contains("open")) this.classList.add("open-active");
-  else this.classList.remove("open-active");
+// card 展開，才添加 open-active class
+function expandPanel(e) {
+  if (e.propertyName !== "font-size") return;
+  selectNode(e.target).classList.toggle("open-active");
 }
