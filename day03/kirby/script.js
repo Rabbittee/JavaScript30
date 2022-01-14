f27c8016992dc1e7aa7f6fdce033150c6fdb1208
Hello Kayac <egg734631@gmail.com>
2022-01-15 02:45:50 +0800

    fix: 🐛 fix init and missing element (#107)

--- day03/kirby/script.js
+++ day03/kirby/script.js
@@ -1,8 +1,17 @@
-var select = function (query) { return document.querySelector(query); };
+var select = function (query) {
+    return document.querySelector(query);
+};
+var selectAll = function (query) {
+    return document.querySelectorAll(query);
+};
 var setProperty = function (element) { return function (property, value) { return element.style.setProperty("--".concat(property), value); }; };
+var update = function (target) {
+    return setProperty(select("body"))(target.name, "".concat(target.value).concat(target.dataset.sizing || ""));
+};
+selectAll(".controls input").forEach(update);
 select(".controls").addEventListener("input", function (_a) {
     var target = _a.target;
     if (!(target instanceof HTMLInputElement))
         return;
-    setProperty(select("img"))(target.name, "".concat(target.value).concat(target.dataset.sizing || ""));
+    update(target);
 });
