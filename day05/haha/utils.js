63413199ff9c95831bfd3170c9b6666794f81d0e
haha861924 <qq861924@gmail.com>
2022-01-15 03:17:59 +0800

    refactor: 💡 click panel

--- day05/haha/utils.js
+++ day05/haha/utils.js
@@ -1,9 +1,8 @@
 const panel = document.querySelector(".panels");
+const selectNode = (target) =>
+  target.nodeName === "DIV" ? target : target.parentNode;
 
-panels.forEach((el) => {
-  // 監聽按下
-  el.addEventListener("click", clickToggle);
-});
+panel.addEventListener("click", clickPanel, false);
 
 panels.forEach((el) => {
   // 監聽動畫結束
@@ -13,6 +12,9 @@ panels.forEach((el) => {
 // 展開收合 card ( toggle 自動切換)
 function clickToggle() {
   this.classList.toggle("open");
+// 展開收合 card，添加 open class
+function clickPanel({ target }) {
+  selectNode(target).classList.toggle("open");
 }
 
 // class 包不包含 open 決定 open-active 的去留
