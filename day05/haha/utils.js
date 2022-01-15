464d0ac07516f756c7ae31085cc1e4a17b3566fe
haha861924 <qq861924@gmail.com>
2022-01-15 17:15:10 +0800

    style: 💄 format

--- day05/haha/utils.js
+++ day05/haha/utils.js
@@ -1,17 +1,16 @@
-const panel = document.querySelector(".panels");
-const selectNode = (target) =>
-  target.nodeName === "DIV" ? target : target.parentNode;
+const panel = document.querySelector('.panels');
+const selectNode = (target) => (target.nodeName === 'DIV' ? target : target.parentNode);
 
-panel.addEventListener("click", clickPanel, false);
-panel.addEventListener("transitionend", expandPanel, false);
+panel.addEventListener('click', clickPanel, false);
+panel.addEventListener('transitionend', expandPanel, false);
 
 // 展開收合 card，添加 open class
 function clickPanel({ target }) {
-  selectNode(target).classList.toggle("open");
+  selectNode(target).classList.toggle('open');
 }
 
 // card 展開，才添加 open-active class
 function expandPanel(e) {
-  if (e.propertyName !== "font-size") return;
-  selectNode(e.target).classList.toggle("open-active");
+  if (e.propertyName !== 'font-size') return;
+  selectNode(e.target).classList.toggle('open-active');
 }
