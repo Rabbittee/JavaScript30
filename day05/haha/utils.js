26d3573e63ce7a970c5c7e4a56d1021e03d19411
haha861924 <qq861924@gmail.com>
2022-01-16 17:30:18 +0800

    refactor: 💡 using closest method to find node

--- day05/haha/utils.js
+++ day05/haha/utils.js
@@ -5,11 +5,11 @@ panels.addEventListener('transitionend', expandPanel, false);
 
 // 展開收合 card，添加 open class
 function clickPanel({ target }) {
-  selectNode(target).classList.toggle('open');
+  target.closest('.panel').classList.toggle('open');
 }
 
 // card 展開，才添加 open-active class
 function expandPanel(e) {
   if (e.propertyName !== 'font-size') return;
-  selectNode(e.target).classList.toggle('open-active');
+  e.target.classList.toggle('open-active');
 }
