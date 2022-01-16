4dc34e74f8b6355a9f46968ab9d34af8080f0d55
Kimberly <work223305@gmail.com>
2022-01-16 17:25:53 +0800

    feat: 🎸 add day04 kim
    add index.js and fix branch name.

--- day04/kim/index.js
+++ day04/kim/index.js
@@ -3,6 +3,7 @@
 
 // Some data we can work with
 const inventors = [
+<<<<<<< HEAD
   { first: 'Albert', last: 'Einstein', year: 1879, passed: 1955 },
   { first: 'Isaac', last: 'Newton', year: 1643, passed: 1727 },
   { first: 'Galileo', last: 'Galilei', year: 1564, passed: 1642 },
@@ -59,25 +60,92 @@ const people = [
   'Blair, Tony',
   'Beecher, Henry',
   'Biondo, Frank',
+=======
+    { first: "Albert", last: "Einstein", year: 1879, passed: 1955 },
+    { first: "Isaac", last: "Newton", year: 1643, passed: 1727 },
+    { first: "Galileo", last: "Galilei", year: 1564, passed: 1642 },
+    { first: "Marie", last: "Curie", year: 1867, passed: 1934 },
+    { first: "Johannes", last: "Kepler", year: 1571, passed: 1630 },
+    { first: "Nicolaus", last: "Copernicus", year: 1473, passed: 1543 },
+    { first: "Max", last: "Planck", year: 1858, passed: 1947 },
+    { first: "Katherine", last: "Blodgett", year: 1898, passed: 1979 },
+    { first: "Ada", last: "Lovelace", year: 1815, passed: 1852 },
+    { first: "Sarah E.", last: "Goode", year: 1855, passed: 1905 },
+    { first: "Lise", last: "Meitner", year: 1878, passed: 1968 },
+    { first: "Hanna", last: "Hammarström", year: 1829, passed: 1909 },
+];
+
+const people = [
+    "Bernhard, Sandra",
+    "Bethea, Erin",
+    "Becker, Carl",
+    "Bentsen, Lloyd",
+    "Beckett, Samuel",
+    "Blake, William",
+    "Berger, Ric",
+    "Beddoes, Mick",
+    "Beethoven, Ludwig",
+    "Belloc, Hilaire",
+    "Begin, Menachem",
+    "Bellow, Saul",
+    "Benchley, Robert",
+    "Blair, Robert",
+    "Benenson, Peter",
+    "Benjamin, Walter",
+    "Berlin, Irving",
+    "Benn, Tony",
+    "Benson, Leana",
+    "Bent, Silas",
+    "Berle, Milton",
+    "Berry, Halle",
+    "Biko, Steve",
+    "Beck, Glenn",
+    "Bergman, Ingmar",
+    "Black, Elk",
+    "Berio, Luciano",
+    "Berne, Eric",
+    "Berra, Yogi",
+    "Berry, Wendell",
+    "Bevan, Aneurin",
+    "Ben-Gurion, David",
+    "Bevel, Ken",
+    "Biden, Joseph",
+    "Bennington, Chester",
+    "Bierce, Ambrose",
+    "Billings, Josh",
+    "Birrell, Augustine",
+    "Blair, Tony",
+    "Beecher, Henry",
+    "Biondo, Frank",
+>>>>>>> bcf2d14 (feat: 🎸 add day04 kim)
 ];
 
 // Array.prototype.filter()
 // 1. Filter the list of inventors for those who were born in the 1500's
 const fifteen = inventors.filter((inventors) => {
+<<<<<<< HEAD
   return inventors.year >= 1500 && inventors.year <= 1600;
+=======
+    return inventors.year >= 1500 && inventors.year <= 1600;
+>>>>>>> bcf2d14 (feat: 🎸 add day04 kim)
 });
 console.table(fifteen);
 
 // Array.prototype.map()
 // 2. Give us an array of the inventors first and last names
 const arrayName = inventors.map((element) => {
+<<<<<<< HEAD
   return element.first + ' ' + element.last;
+=======
+    return element.first + " " + element.last;
+>>>>>>> bcf2d14 (feat: 🎸 add day04 kim)
 });
 console.table(arrayName);
 
 // Array.prototype.sort()
 // 3. Sort the inventors by birthdate, oldest to youngest
 // Solution 01
+<<<<<<< HEAD
 const birthdayOne = inventors.sort((a, b) => {
   if (a.year < b.year) {
     return -1;
@@ -92,18 +160,39 @@ const birthdayTwo = inventors.sort((a, b) => {
   return a.year < b.year ? -1 : 1;
 });
 console.table(birthdayTwo);
+=======
+const birthday = inventors.sort((a, b) => {
+    if (a.year < b.year) {
+        return -1;
+    } else {
+        1;
+    }
+});
+
+// Solution 02
+const birthday = inventors.sort((a, b) => {
+    return a.year < b.year ? -1 : 1;
+});
+console.table(birthday);
+>>>>>>> bcf2d14 (feat: 🎸 add day04 kim)
 
 // Array.prototype.reduce()
 // 4. How many years did all the inventors live all together?
 const sum = inventors.reduce((acc, cur) => {
+<<<<<<< HEAD
   const singleYear = cur.passed - cur.year;
   return (acc = acc + singleYear);
+=======
+    const singleYear = cur.passed - cur.year;
+    return (acc = acc + singleYear);
+>>>>>>> bcf2d14 (feat: 🎸 add day04 kim)
 }, 0);
 console.log(sum);
 
 // 5. Sort the inventors by years lived
 // Step1. Calculate years lived
 const sortYear = inventors.forEach((e) => {
+<<<<<<< HEAD
   const count = e.passed - e.year;
   // Step2. Create object with value
   e.HowOld = count;
@@ -111,23 +200,41 @@ const sortYear = inventors.forEach((e) => {
 // Step3. Sort old to young
 inventors.sort(function (a, b) {
   return b.HowOld - a.HowOld;
+=======
+    const count = e.passed - e.year;
+    // Step2. Create object with value
+    e.HowOld = count;
+});
+// Step3. Sort old to young
+inventors.sort(function (a, b) {
+    return b.HowOld - a.HowOld;
+>>>>>>> bcf2d14 (feat: 🎸 add day04 kim)
 });
 console.table(inventors);
 
 // 7. sort Exercise
 // Sort the people alphabetically by last name
 const lastName = inventors.sort(function (a, b) {
+<<<<<<< HEAD
   const nameA = a.last;
   const nameB = b.last;
 
   // Sort A to Z
   return nameA > nameB ? 1 : -1;
+=======
+    const nameA = a.last;
+    const nameB = b.last;
+
+    // Sort A to Z
+    return nameA > nameB ? 1 : -1;
+>>>>>>> bcf2d14 (feat: 🎸 add day04 kim)
 });
 console.table(lastName);
 
 // 8. Reduce Exercise
 // Sum up the instances of each of these
 const data = [
+<<<<<<< HEAD
   'car',
   'car',
   'truck',
@@ -151,5 +258,30 @@ const countedNames = data.reduce(function (allNames, name) {
     allNames[name] = 1;
   }
   return allNames;
+=======
+    "car",
+    "car",
+    "truck",
+    "truck",
+    "bike",
+    "walk",
+    "car",
+    "van",
+    "bike",
+    "walk",
+    "car",
+    "van",
+    "car",
+    "truck",
+];
+
+const countedNames = data.reduce(function (allNames, name) {
+    if (name in allNames) {
+        allNames[name]++;
+    } else {
+        allNames[name] = 1;
+    }
+    return allNames;
+>>>>>>> bcf2d14 (feat: 🎸 add day04 kim)
 }, {});
 console.log(countedNames);
