581efdb41b705c500d081e1f125a6c7c7aef6ec3
Peter Yeh <leev126g8@gmail.com>
2022-01-17 15:45:59 +0800

    perf: ⚡️ use toggle improve code

--- day05/recoil/script.js
+++ day05/recoil/script.js
@@ -1,15 +1,12 @@
 const $ = (target) => document.querySelectorAll(target);
 
-const panels = $(".panel");
+const panels = $('.panel');
 
 panels.forEach(function (element) {
   element.addEventListener('click', function ({ target: { classList } }) {
     classList.toggle('open');
   });
-  element.addEventListener(
-    'transitionend',
-    function ({ target: { classList } }) {
-      classList.contains("open") ? classList.add("open-active") : classList.remove("open-active");
-    }
-  );
+  element.addEventListener('transitionend', function ({ target: { classList } }) {
+    classList.toggle('open-active', classList.contains('open'));
+  });
 });
