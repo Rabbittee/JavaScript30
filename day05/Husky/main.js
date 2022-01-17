e5c87799237eff82d9a880c81d58f1300d634316
HuskyHsu <schi.hsu@gmail.com>
2022-01-18 00:22:30 +0800

    feat: 🎸 update panel backgroud image

--- day05/Husky/main.js
+++ day05/Husky/main.js
@@ -1,21 +1,24 @@
-const titles = [
-  ['Hey', "Let's", 'Dance'],
-  ['Give', 'Take', 'Receive'],
-  ['Experience', 'It', 'Today'],
-  ['Give', 'All', 'You can'],
-];
+const titles = ['我妻善逸', '竈門禰豆子', '竈門炭治郎', '嘴平伊之助'];
 
 const app = document.getElementById('app');
 app.classList.add('flex', 'w-full');
 const titlesDom = titles.map((title, index) => {
   const div = document.createElement('div');
-  div.classList.add(...[`panel${index + 1}`, 'w-full', `bg-cover`, `bg-left`]);
+  div.classList.add(
+    ...[
+      'panel',
+      `panel${index + 1}`,
+      'w-full',
+      `before:bg-cover`,
+      `before:bg-left`,
+      `before:grayscale`,
+    ]
+  );
 
-  title.forEach((item) => {
-    const p = document.createElement('p');
-    p.innerText = item;
-    div.appendChild(p);
-  });
+  const p = document.createElement('p');
+  // p.innerText = '';
+  p.style.backgroundImage = `url(./images/${title}.png)`;
+  div.appendChild(p);
 
   app.appendChild(div);
   return div;
