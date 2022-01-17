5a27f6d54a59e5a990a8ec0fc5fedd1c8a01b944
kevinhws <kevinshu1995@gmail.com>
2022-01-17 23:50:34 +0800

    style: 💄 prettier format

--- day05/kevin/index.js
+++ day05/kevin/index.js
@@ -1,12 +1,12 @@
-document.querySelector(".panels").addEventListener("click", (e) => {
-  const clickedPanel = e.target.closest(".panel");
-  const hasOpened = clickedPanel.classList.contains("open");
+document.querySelector('.panels').addEventListener('click', (e) => {
+  const clickedPanel = e.target.closest('.panel');
+  const hasOpened = clickedPanel.classList.contains('open');
 
   const transitionHandler = () => {
-    clickedPanel.classList.toggle("active", !hasOpened);
-    clickedPanel.removeEventListener("transitionend", transitionHandler);
+    clickedPanel.classList.toggle('active', !hasOpened);
+    clickedPanel.removeEventListener('transitionend', transitionHandler);
   };
 
-  clickedPanel.classList.toggle("open");
-  clickedPanel.addEventListener("transitionend", transitionHandler);
+  clickedPanel.classList.toggle('open');
+  clickedPanel.addEventListener('transitionend', transitionHandler);
 });
