70688f47838775e3ec7c22541d04cfe89b7ae508
beareatapple <beareatapple@gmail.com>
2022-01-17 09:07:50 +0800

    fixed white's format bug

--- day03/White/script.js
+++ day03/White/script.js
@@ -1,7 +1,7 @@
 function updateCss(id) {
-    const value = document.getElementById(id).value;
-    const unit = document.getElementById(id).dataset.sizing == 'px' ? 'px' : '';
+  const value = document.getElementById(id).value;
+  const unit = document.getElementById(id).dataset.sizing == 'px' ? 'px' : '';
 
-    // 更新CSS變數值 此處命名與id名稱相同
-    document.documentElement.style.setProperty(`--${id}`, `${value}${unit}`);
+  // 更新CSS變數值 此處命名與id名稱相同
+  document.documentElement.style.setProperty(`--${id}`, `${value}${unit}`);
 }
