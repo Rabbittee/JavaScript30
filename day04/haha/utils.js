dca9bfe08ceffe966a0a00b10616d1073131099a
haha861924 <qq861924@gmail.com>
2022-01-18 09:35:57 +0800

    style: 💄 format

--- day04/haha/utils.js
+++ day04/haha/utils.js
@@ -1,124 +1,102 @@
 const inventors = [
-  { first: "Albert", last: "Einstein", year: 1879, passed: 1955 },
-  { first: "Isaac", last: "Newton", year: 1643, passed: 1727 },
-  { first: "Galileo", last: "Galilei", year: 1564, passed: 1642 },
-  { first: "Marie", last: "Curie", year: 1867, passed: 1934 },
-  { first: "Johannes", last: "Kepler", year: 1571, passed: 1630 },
-  { first: "Nicolaus", last: "Copernicus", year: 1473, passed: 1543 },
-  { first: "Max", last: "Planck", year: 1858, passed: 1947 },
-  { first: "Katherine", last: "Blodgett", year: 1898, passed: 1979 },
-  { first: "Ada", last: "Lovelace", year: 1815, passed: 1852 },
-  { first: "Sarah E.", last: "Goode", year: 1855, passed: 1905 },
-  { first: "Lise", last: "Meitner", year: 1878, passed: 1968 },
-  { first: "Hanna", last: "Hammarström", year: 1829, passed: 1909 },
+  { first: 'Albert', last: 'Einstein', year: 1879, passed: 1955 },
+  { first: 'Isaac', last: 'Newton', year: 1643, passed: 1727 },
+  { first: 'Galileo', last: 'Galilei', year: 1564, passed: 1642 },
+  { first: 'Marie', last: 'Curie', year: 1867, passed: 1934 },
+  { first: 'Johannes', last: 'Kepler', year: 1571, passed: 1630 },
+  { first: 'Nicolaus', last: 'Copernicus', year: 1473, passed: 1543 },
+  { first: 'Max', last: 'Planck', year: 1858, passed: 1947 },
+  { first: 'Katherine', last: 'Blodgett', year: 1898, passed: 1979 },
+  { first: 'Ada', last: 'Lovelace', year: 1815, passed: 1852 },
+  { first: 'Sarah E.', last: 'Goode', year: 1855, passed: 1905 },
+  { first: 'Lise', last: 'Meitner', year: 1878, passed: 1968 },
+  { first: 'Hanna', last: 'Hammarström', year: 1829, passed: 1909 },
 ];
 
 const people = [
-  "Bernhard, Sandra",
-  "Bethea, Erin",
-  "Becker, Carl",
-  "Bentsen, Lloyd",
-  "Beckett, Samuel",
-  "Blake, William",
-  "Berger, Ric",
-  "Beddoes, Mick",
-  "Beethoven, Ludwig",
-  "Belloc, Hilaire",
-  "Begin, Menachem",
-  "Bellow, Saul",
-  "Benchley, Robert",
-  "Blair, Robert",
-  "Benenson, Peter",
-  "Benjamin, Walter",
-  "Berlin, Irving",
-  "Benn, Tony",
-  "Benson, Leana",
-  "Bent, Silas",
-  "Berle, Milton",
-  "Berry, Halle",
-  "Biko, Steve",
-  "Beck, Glenn",
-  "Bergman, Ingmar",
-  "Black, Elk",
-  "Berio, Luciano",
-  "Berne, Eric",
-  "Berra, Yogi",
-  "Berry, Wendell",
-  "Bevan, Aneurin",
-  "Ben-Gurion, David",
-  "Bevel, Ken",
-  "Biden, Joseph",
-  "Bennington, Chester",
-  "Bierce, Ambrose",
-  "Billings, Josh",
-  "Birrell, Augustine",
-  "Blair, Tony",
-  "Beecher, Henry",
-  "Biondo, Frank",
+  'Bernhard, Sandra',
+  'Bethea, Erin',
+  'Becker, Carl',
+  'Bentsen, Lloyd',
+  'Beckett, Samuel',
+  'Blake, William',
+  'Berger, Ric',
+  'Beddoes, Mick',
+  'Beethoven, Ludwig',
+  'Belloc, Hilaire',
+  'Begin, Menachem',
+  'Bellow, Saul',
+  'Benchley, Robert',
+  'Blair, Robert',
+  'Benenson, Peter',
+  'Benjamin, Walter',
+  'Berlin, Irving',
+  'Benn, Tony',
+  'Benson, Leana',
+  'Bent, Silas',
+  'Berle, Milton',
+  'Berry, Halle',
+  'Biko, Steve',
+  'Beck, Glenn',
+  'Bergman, Ingmar',
+  'Black, Elk',
+  'Berio, Luciano',
+  'Berne, Eric',
+  'Berra, Yogi',
+  'Berry, Wendell',
+  'Bevan, Aneurin',
+  'Ben-Gurion, David',
+  'Bevel, Ken',
+  'Biden, Joseph',
+  'Bennington, Chester',
+  'Bierce, Ambrose',
+  'Billings, Josh',
+  'Birrell, Augustine',
+  'Blair, Tony',
+  'Beecher, Henry',
+  'Biondo, Frank',
 ];
 
 const filterYear = inventors.filter((el) => el.year >= 1500 && el.year < 1600);
-console.log(
-  "1. Filter the list of inventors for those who were born in the 1500's",
-  filterYear
-);
+console.log("1. Filter the list of inventors for those who were born in the 1500's", filterYear);
 
 const mapName = inventors.map((el) => `${el.first} ${el.last}`);
-console.log(
-  "2. Give us an array of the inventors first and last names",
-  mapName
-);
+console.log('2. Give us an array of the inventors first and last names', mapName);
 
 const sortBirthdate = inventors.sort((a, b) => a.year - b.year);
-console.log(
-  "3. Sort the inventors by birthdate, oldest to youngest",
-  sortBirthdate
-);
+console.log('3. Sort the inventors by birthdate, oldest to youngest', sortBirthdate);
 
-const reduceLive = inventors.reduce(
-  (sum, curr) => sum + (curr.passed - curr.year),
-  0
-);
-console.log(
-  "4. How many years did all the inventors live all together?",
-  reduceLive
-);
+const reduceLive = inventors.reduce((sum, curr) => sum + (curr.passed - curr.year), 0);
+console.log('4. How many years did all the inventors live all together?', reduceLive);
 
-const sortLived = inventors.sort(
-  (a, b) => b.passed - b.year - (a.passed - a.year)
-);
-console.log("5. Sort the inventors by years lived", sortLived);
+const sortLived = inventors.sort((a, b) => b.passed - b.year - (a.passed - a.year));
+console.log('5. Sort the inventors by years lived', sortLived);
 
 // 6. create a list of Boulevards in Paris that contain 'de' anywhere in the name
 // https://en.wikipedia.org/wiki/Category:Boulevards_in_Paris
 // skip it
-const list = people.filter((el) => el.includes("de"));
-console.log("6. contain `de` anywhere in the people", list);
+const list = people.filter((el) => el.includes('de'));
+console.log('6. contain `de` anywhere in the people', list);
 
-const splitName = (name) => name.split(", ")[0];
-const sortLastName = people.sort((next, curr) =>
-  splitName(next) > splitName(curr) ? 1 : -1
-);
-console.log(
-  "7. sort Exercise: Sort the people alphabetically by last name",
-  sortLastName
-);
+const splitName = (name) => name.split(', ')[0];
+const sortLastName = people.sort((next, curr) => (splitName(next) > splitName(curr) ? 1 : -1));
+console.log('7. sort Exercise: Sort the people alphabetically by last name', sortLastName);
 
 const data = [
-  "car",
-  "car",
-  "truck",
-  "truck",
-  "bike",
-  "walk",
-  "car",
-  "van",
-  "bike",
-  "walk",
-  "car",
-  "van",
-  "car",
-  "truck",
+  'car',
+  'car',
+  'truck',
+  'truck',
+  'bike',
+  'walk',
+  'car',
+  'van',
+  'bike',
+  'walk',
+  'car',
+  'van',
+  'car',
+  'truck',
 ];
 
 const countTimes = data.reduce((obj, curr) => {
@@ -128,7 +106,4 @@ const countTimes = data.reduce((obj, curr) => {
   obj[curr]++;
   return obj;
 }, {});
-console.log(
-  "8. Reduce Exercise: Sum up the instances of each of these",
-  countTimes
-);
+console.log('8. Reduce Exercise: Sum up the instances of each of these', countTimes);
