4805069eb766cea2ba9af4429db2582cd3f1a504
HuskyHsu <schi.hsu@gmail.com>
2022-01-19 15:32:00 +0800

    docs: ✏️ add day dashboard

--- script/getLink.py
+++ script/getLink.py
@@ -2,28 +2,31 @@ from os import listdir
 from os.path import isfile, join
 from typing import List, TypedDict
 import re
-
+from unicodedata import name
 
 BASE_URL = 'https://rabbittee.github.io/JavaScript30/'
-USER_ALIAS = {
-    'alpha': '阿法',
-    'cola': '可樂',
-    'erica': '鳥胃',
-    'elzuoc': '轉圈圈',
-    'haha': '吠吠',
-    'husky': '哈奇',
-    'kevin': '凱文',
-    'kim': '阿金',
-    'kirby': '哈囉',
-    'pencil': '鉛筆',
-    'plusfive': '+5',
-    'recoil': '奶捲',
-    'white': '小白'
+
+USER_INFO = {
+    'alpha': {'alias': '阿法', 'avatar_id': '46735230'},
+    'cola': {'alias': '可樂', 'avatar_id': '3334950'},
+    'erica': {'alias': '鳥胃', 'avatar_id': '53078636'},
+    'elzuoc': {'alias': '轉圈圈', 'avatar_id': '68758083'},
+    'haha': {'alias': '吠吠', 'avatar_id': '40877568'},
+    'husky': {'alias': '哈奇', 'avatar_id': '18141436'},
+    'kevin': {'alias': '凱文', 'avatar_id': '56685204'},
+    'kim': {'alias': '阿金', 'avatar_id': '87320011'},
+    'kirby': {'alias': '哈囉', 'avatar_id': '32451546'},
+    'pencil': {'alias': '鉛筆', 'avatar_id': '49506360'},
+    'plusfive': {'alias': '+5', 'avatar_id': '75515395'},
+    'recoil': {'alias': '奶捲', 'avatar_id': '40841221'},
+    'white': {'alias': '小白', 'avatar_id': '82199955'}
 }
 
 
 class LinkDict(TypedDict):
-    user: str
+    id: str
+    name: str
+    avatar_id: str
     path: str
 
 
@@ -33,23 +36,28 @@ def get_day_links(day_path: str) -> List[LinkDict]:
 
     for dir_name in users_dir:
         user_full_path = join(day_path, dir_name)
-        if isfile(user_full_path) or dir_name == 'sample':
+        index_path = join(day_path, dir_name, 'index.html')
+        if isfile(user_full_path) or dir_name == 'sample' or not isfile(index_path):
             continue
 
-        alias = USER_ALIAS.get(dir_name.lower(), dir_name)
+        info = USER_INFO.get(
+            dir_name.lower(),
+            {'alias': dir_name, 'avatar_id': ''}
+        )
 
         dist_index_path = join(day_path, dir_name, 'dist', 'index.html')
-        index_path = join(day_path, dir_name, 'index.html')
+
+        user = {
+            'id': dir_name,
+            'name': info['alias'],
+            'avatar_id': info['avatar_id'],
+            'path': f'{BASE_URL}{user_full_path}/'
+        }
         if isfile(dist_index_path):
-            links.append({
-                'user': alias,
-                'path': f'{BASE_URL}{user_full_path}/dist/'
-            })
-        elif isfile(index_path):
-            links.append({
-                'user': alias,
-                'path': f'{BASE_URL}{user_full_path}/'
-            })
+            user['path'] += 'dist/'
+
+        links.append(user)
+
     links.sort(key=lambda i: i['path'])
     return links
 
@@ -57,12 +65,12 @@ def get_day_links(day_path: str) -> List[LinkDict]:
 def create_markdown_text(users_link: List[LinkDict]) -> str:
     output = '## Links\n\n'
     for user_link in users_link:
-        output += f'- [{user_link["user"]}]({user_link["path"]})\n'
+        output += f'- [{user_link["name"]}]({user_link["path"]})\n'
 
     return output
 
 
-def update_markdown(day_path: str, links_text: str):
+def update_markdown(day_path: str, links_text: str) -> None:
     output = ''
     try:
         with open(f'{day_path}/README.md', 'rt') as fin:
@@ -81,6 +89,44 @@ def update_markdown(day_path: str, links_text: str):
         fout.write(f'{output}\n\n{links_text}')
 
 
+def gen_avatar_link(user: LinkDict) -> str:
+    if user['avatar_id'] == '':
+        return f'https://ui-avatars.com/api/?background=random&name={user["id"]}'
+    return f'https://avatars.githubusercontent.com/u/{user["avatar_id"]}?s=60&v=4'
+
+
+def gen_dashboard_item(user: LinkDict) -> str:
+    with open(f'script/day_dashboard_item.template', 'rt') as fin:
+        item_str = fin.read()
+
+    info = {
+        'id': user['id'],
+        'name': user['name'],
+        'link_url': user['path'],
+        'link_avatar': gen_avatar_link(user)
+    }
+
+    for key, value in info.items():
+        item_str = item_str.replace(f'{{{{{key}}}}}', value)
+
+    return item_str
+
+
+def gen_dashboard(day_path: str, users_link: List[LinkDict]) -> None:
+    dashboard_item = '\n'.join(
+        [gen_dashboard_item(user_link) for user_link in users_link]
+    )
+
+    with open(f'script/day_dashboard.template', 'rt') as fin:
+        dashboard_str = fin.read()
+
+    dashboard_str = dashboard_str.replace(
+        '{{items}}', dashboard_item).replace('{{day}}', day_path[-2:])
+
+    with open(f'{day_path}/index.html', 'wt') as fout:
+        fout.write(dashboard_str)
+
+
 if __name__ == '__main__':
 
     for i in range(1, 31):
@@ -90,3 +136,4 @@ if __name__ == '__main__':
             continue
         markdown_text = create_markdown_text(users_link)
         update_markdown(day, markdown_text)
+        gen_dashboard(day, users_link)
