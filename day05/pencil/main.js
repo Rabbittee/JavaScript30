3b4f76de7a6680bb8d5ce2d8ff8565da20756ee4
zoeGuava <zoeguava@gmail.com>
2022-01-20 02:28:16 +0800

    feat: add pencil's day05 solution

--- day05/pencil/main.js
+++ day05/pencil/main.js
@@ -1,7 +1,9 @@
+/** query selector function */
 function select(className) {
   return document.querySelectorAll(`${className}`);
 }
 
+/** toggle the open class */
 function toggleStatus(e) {
   const panelElement = e.target.parentNode;
   const statusOpen = panelElement.classList.contains('open');
@@ -14,6 +16,7 @@ function toggleStatus(e) {
   panelElement.classList.add('open');
 }
 
+/** add event listeners*/
 select('.panels').forEach((element) => {
   element.addEventListener('click', toggleStatus);
 });
