694ae6531202d30ead2d849759bcea4be4fae5bd
zoeGuava <zoeguava@gmail.com>
2022-01-20 01:03:18 +0800

    feat: add toggle status function

--- day05/pencil/main.js
+++ day05/pencil/main.js
@@ -1,3 +1,19 @@
-console.log('day05 page');
+function select(className) {
+  return document.querySelectorAll(`${className}`);
+}
 
-const container = document.querySelector('.panels');
+function toggleStatus(e) {
+  const panelElement = e.target.parentNode;
+  const statusOpen = panelElement.classList.contains('open');
+
+  if (statusOpen) {
+    panelElement.classList.remove('open');
+    return;
+  }
+
+  panelElement.classList.add('open');
+}
+
+select('.panels').forEach((element) => {
+  element.addEventListener('click', toggleStatus);
+});
