8f1bbfd9149051e2303aa24df5096899f0420827
Hello Kayac <egg734631@gmail.com>
2022-01-19 09:31:15 +0800

    feat: 🎸 add day05 kirby (#20)
    * feat: 🎸 add day05 kirby
    
    * fix: 🐛 fix animation delay

---
+++ day05/kirby/script.js
@@ -0,0 +1,8 @@
+var select = function (query) { return document.querySelector(query); };
+select(".panels").addEventListener("click", function (_a) {
+    var _b;
+    var target = _a.target;
+    if (!(target instanceof Element))
+        return;
+    (_b = target.closest(".panel")) === null || _b === void 0 ? void 0 : _b.classList.toggle("open");
+});
