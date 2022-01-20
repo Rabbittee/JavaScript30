b95974701d3b57055959e94611ea5ea407d2f196
Claire Li <claire.li@singularinfinity.com.tw>
2022-01-20 11:44:15 +0800

    format

--- day05/plusfive/main.js
+++ day05/plusfive/main.js
@@ -1,16 +1,15 @@
-const panels = document.querySelectorAll(".panel");
+const panels = document.querySelectorAll('.panel');
 
 panels.forEach((panel) => {
-  panel.addEventListener("click", toggleOpen);
-  panel.addEventListener("transitionend", toggleTransition);
+  panel.addEventListener('click', toggleOpen);
+  panel.addEventListener('transitionend', toggleTransition);
 });
 
 function toggleOpen() {
-  this.classList.toggle("open");
+  this.classList.toggle('open');
 }
 
 function toggleTransition(e) {
   // this.classList.toggle('open-active')
-  if (e.propertyName.includes("flex-grow"))
-    this.classList.toggle("open-active");
+  if (e.propertyName.includes('flex-grow')) this.classList.toggle('open-active');
 }
