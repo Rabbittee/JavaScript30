ab3e8085340f3fe1c93b7fea5d96d05efbfe1411
Claire Li <claire.li@singularinfinity.com.tw>
2022-01-20 11:38:14 +0800

    Feat: add day05 plusfive's answer

---
+++ day05/plusfive/main.js
@@ -0,0 +1,16 @@
+const panels = document.querySelectorAll(".panel");
+
+panels.forEach((panel) => {
+  panel.addEventListener("click", toggleOpen);
+  panel.addEventListener("transitionend", toggleTransition);
+});
+
+function toggleOpen() {
+  this.classList.toggle("open");
+}
+
+function toggleTransition(e) {
+  // this.classList.toggle('open-active')
+  if (e.propertyName.includes("flex-grow"))
+    this.classList.toggle("open-active");
+}
