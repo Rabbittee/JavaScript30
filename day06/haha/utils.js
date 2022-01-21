74bf85affbe70803b357ca985571c7ef73fa3128
haha861924 <qq861924@gmail.com>
2022-01-21 15:45:55 +0800

    style: 💄 format

--- day06/haha/utils.js
+++ day06/haha/utils.js
@@ -1,16 +1,16 @@
 const endpoint =
-  "https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json";
+  'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
 
-const input = document.querySelector("input");
+const input = document.querySelector('input');
 
-input.addEventListener("focus", clearContent);
-input.addEventListener("blur", testInputVal);
+input.addEventListener('focus', clearContent);
+input.addEventListener('blur', testInputVal);
 
 function clearContent() {
-  this.setAttribute("placeholder", "");
+  this.setAttribute('placeholder', '');
 }
 
 function testInputVal() {
-  const text = this.value === "" ? "City or State" : this.value;
-  this.setAttribute("placeholder", text);
+  const text = this.value === '' ? 'City or State' : this.value;
+  this.setAttribute('placeholder', text);
 }
