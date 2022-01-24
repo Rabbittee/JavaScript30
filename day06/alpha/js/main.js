3c390a0c04afb5c571360adca402fa49b8222127
Alpha <alphatero@icloud.com>
2022-01-24 16:07:02 +0800

    feat: 🎸 finish js solution

--- day06/alpha/js/main.js
+++ day06/alpha/js/main.js
@@ -1,15 +1,50 @@
 import { fetchData } from './fetch.js';
+import './header.js';
 
 const data = await fetchData();
 
-const search = document.querySelector('.search');
+const select = (element) => document.querySelector(element);
 
-const result = document.querySelector('.result');
+const init = () => (select('.result').innerHTML = ` <li class="list-item">No result</li>`);
 
-search.addEventListener('input', (e) => {
-  console.log(e.target.value);
-});
+/**
+ *
+ * @param {string} input
+ * @param {Object} data
+ * @returns
+ */
+const matchKey = (input, data) => {
+  const reg = new RegExp(input, 'gi');
+  if (input === '') return;
+  return data.city.match(reg) || data.state.match(reg);
+};
 
-const current = data.filter((item) => item.city.match('N'));
+const formatNumber = (number) => number.replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ',');
 
-console.log(current);
+/**
+ * 取得符合關鍵字的物件
+ * @param {InputEvent} e
+ * @returns
+ */
+const getMatch = (e) => {
+  const keyWord = e.target.value;
+
+  const current = data.filter((item) => matchKey(keyWord, item));
+
+  if (current.length === 0) return init();
+  const html = current
+    .map((item) => {
+      const { state, city, population } = item;
+
+      const populationFormated = formatNumber(population);
+
+      return `
+    <li class="list-item">${city}, ${state} <small>${populationFormated}</small></li>
+    `;
+    })
+    .join('');
+
+  select('.result').innerHTML = html;
+};
+
+select('.search').addEventListener('input', getMatch);
