aac1a529ad892b812d420c6410ea6ddfb4a0d49d
Hello Kayac <egg734631@gmail.com>
2022-01-25 23:19:45 +0800

    Feature/day06 kirby (#172)
    * feat: 🎸 add day06 kirby
    
    * fix: 🐛 fix formatting
    
    * fix: 🐛 fix formatting issue
    
    * fix: 🐛 fix prettier formatting issue

---
+++ day06/kirby/script.js
@@ -0,0 +1,101 @@
+var endpoint =
+  'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
+var toJSON = function (res) {
+  return res.json();
+};
+var tap = function (fn) {
+  return function (arg) {
+    return fn(arg), arg;
+  };
+};
+var filter = function (fn) {
+  return function (list) {
+    return list.filter(fn);
+  };
+};
+var map = function (fn) {
+  return function (list) {
+    return list.map(fn);
+  };
+};
+var join = function (sep) {
+  return function (list) {
+    return list.join(sep);
+  };
+};
+var select = function (query) {
+  return document.querySelector(query);
+};
+var render = function (element) {
+  return function (html) {
+    return (element.innerHTML = html);
+  };
+};
+var debounce = function (ms, fn) {
+  var id = undefined;
+  return function (arg) {
+    if (id) clearTimeout(id);
+    id = setTimeout(function () {
+      return fn(arg);
+    }, ms);
+  };
+};
+var memo = function (keyFn, fn) {
+  var cache = {};
+  return function (arg) {
+    var key = keyFn(arg);
+    if (!(key in cache)) cache[key] = fn(arg);
+    return cache[key];
+  };
+};
+var fetchCity = memo(
+  JSON.stringify,
+  function () {
+    return fetch(endpoint).then(toJSON).then(tap(console.log));
+  }
+  //
+);
+var searchWith = function (token) {
+  return function (target) {
+    return RegExp(token, 'ig').test(target);
+  };
+};
+var replaceWith = function (token, replacement) {
+  return function (target) {
+    return target.replace(RegExp('('.concat(token, ')'), 'ig'), replacement);
+  };
+};
+function Record(_a) {
+  var city = _a.city,
+    state = _a.state,
+    population = _a.population;
+  return /*html */ '\n          <li>\n              <span>'
+    .concat(city, ', ')
+    .concat(state, '</span>\n              <span>')
+    .concat(population, '</span>\n          </li>\n      ')
+    .replace(/\n/g, '');
+}
+select('.search').addEventListener(
+  'input',
+  debounce(300, function (_a) {
+    var target = _a.target;
+    if (!(target instanceof HTMLInputElement)) return;
+    if (!target.value) {
+      return render(select('.suggestions'))('');
+    }
+    var search = searchWith(target.value);
+    var replace = replaceWith(target.value, '<mark>$1</mark>');
+    fetchCity()
+      .then(
+        filter(function (_a) {
+          var city = _a.city,
+            state = _a.state;
+          return search(city) || search(state);
+        })
+      )
+      .then(map(Record))
+      .then(join(''))
+      .then(replace)
+      .then(render(select('.suggestions')));
+  })
+);
