8301c3a762cc548c7953a78acb227823dd145524
Erica <erica.du0916@gmail.com>
2022-01-25 21:28:16 +0800

    fix: update solution

--- day06/erica/scripts/fetch.js
+++ day06/erica/scripts/fetch.js
@@ -1,11 +1,3 @@
 const endpoint =
   'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
-
-const cities = [];
-(function () {
-  fetch(endpoint)
-    .then((data) => data.json())
-    .then((data) => cities.push(...data));
-})();
-
-export default cities;
+export const cities = await fetch(endpoint).then((data) => data.json());
--- day06/erica/scripts/index.js
+++ day06/erica/scripts/index.js
@@ -1,14 +1,13 @@
 import { $ } from './utils.js';
-import cities from './fetch.js';
+import { cities } from './fetch.js';
 import updateListing from './update.js';
 
-function matchKeyword() {
-  const keyword = this.value.trim().toLowerCase();
+function matchKeyword(e) {
+  const keyword = e.target.value.trim().toLowerCase();
   let result = [];
 
   if (keyword === '') {
-    result = [];
-    this.value = '';
+    e.target.value = '';
   } else {
     result = cities.filter((data) => {
       const { city, state } = data;
