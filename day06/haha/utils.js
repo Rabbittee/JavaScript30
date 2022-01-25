2ea612259d89ee4b55283cfdfa2444b28e512ea4
Rabbittee <rabbitteetw@gmail.com>
2022-01-25 15:12:35 +0000

    Auto-merge feat/day06-haha

--- day06/haha/utils.js
+++ day06/haha/utils.js
@@ -1,48 +1,33 @@
 const endpoint =
-  "https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json";
+  'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
 const cities = [];
 // 獲取資料
 fetch(endpoint)
   .then((res) => res.json())
   .then((data) => cities.push(...data));
 
-const input = document.querySelector("input");
-
-input.addEventListener("focus", clearContent);
-input.addEventListener("blur", testInputVal);
-input.addEventListener("input", filterKeyWords);
-
-function clearContent() {
-  this.setAttribute("placeholder", "");
-}
-
-function testInputVal() {
-  const text = this.value === "" ? "City or State" : this.value;
-  this.setAttribute("placeholder", text);
-}
+const input = document.querySelector('input');
+input.addEventListener('input', filterKeyWords);
 
 function filterKeyWords() {
   removeChild(); // 搜尋之前先把上一筆子節點清空
+  const regex = new RegExp(input.value, 'gi');
   const result = cities.filter((el) => {
-    return el.city.includes(this.value) || el.state.includes(this.value);
+    return regex.test(el.city) || regex.test(el.state);
   });
   result.forEach((el) => {
     appendChild(el); // 新增子節點
   });
 }
+
 // 新增子節點
 function appendChild(el) {
-  const ul = document.querySelectorAll(".suggestions");
-  const markCity = el.city.replace(
-    input.value,
-    `<span class="hl">${input.value}</span>`
-  );
-  const markState = el.state.replace(
-    input.value,
-    `<span class="hl">${input.value}</span>`
-  );
+  const ul = document.querySelectorAll('.suggestions');
+  const regex = new RegExp(input.value, 'gi');
+  const markCity = el.city.replace(regex, `<span class="hl">${input.value}</span>`);
+  const markState = el.state.replace(regex, `<span class="hl">${input.value}</span>`);
   ul[0].insertAdjacentHTML(
-    "afterbegin",
+    'beforeend',
     `<li>
         <span class="name">${markCity}, ${markState}</span>
         <span class="population">${el.population}</span>
@@ -52,8 +37,6 @@ function appendChild(el) {
 
 // 移除子節點
 function removeChild() {
-  const ul = document.querySelectorAll(".suggestions li");
-  for (let el of ul) {
-    el.remove();
-  }
+  const ul = document.querySelector('.suggestions');
+  ul.innerHTML = '';
 }
