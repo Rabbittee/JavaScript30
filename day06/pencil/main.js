7f490ce46302c92abc845908f373aa79ede2254d
Rabbittee <rabbitteetw@gmail.com>
2022-01-29 17:46:05 +0000

    Auto-merge feature/day06_pencil

---
+++ day06/pencil/main.js
@@ -0,0 +1,45 @@
+const endpoint =
+  'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
+
+const select = function (dom) {
+  return document.querySelector(dom);
+};
+
+const cities = [];
+fetch(endpoint)
+  .then((response) => response.json())
+  .then((data) => {
+    cities.push(...data);
+  })
+  .catch((error) => console.log(`Error: ${error}`));
+
+const filterData = (searchInput, data) => {
+  return data.filter((item) => {
+    return item.city.includes(searchInput) || item.state.includes(searchInput);
+  });
+};
+
+function numberWithCommas(x) {
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+}
+
+const renderData = (e) => {
+  const inputValue = e.target.value;
+  const filteredData = filterData(inputValue, cities);
+  const domStructure = filteredData
+    .map(({ city, state, population }) => {
+      const regex = new RegExp(inputValue, 'gi');
+      const cityName = city.replace(regex, `<span class="hl">${inputValue}</span>`);
+      const stateName = state.replace(regex, `<span class="hl">${inputValue}</span>`);
+      return `
+      <li>
+        <span class="name">${cityName}, ${stateName}</span>
+        <span class="population">${numberWithCommas(population)}</span>
+      </li>
+    `;
+    })
+    .join('');
+  select('.suggestions').innerHTML = domStructure;
+};
+
+select('.search').addEventListener('input', renderData);
