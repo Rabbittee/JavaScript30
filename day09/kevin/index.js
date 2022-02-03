b176d1e0daab4f319b6150e919e46f55415d5fc8
kevinhws <kevinshu1995@gmail.com>
2022-02-03 20:37:32 +0800

    feat: 🎸 add kevin day 09 solution

--- day09/kevin/index.js
+++ day09/kevin/index.js
@@ -10,25 +10,51 @@ function makeGreen() {
 }
 
 // Regular
+console.log('Regular');
 
 // Interpolated
+console.log('Interpolated a word %s !', 'hello');
 
 // Styled
+console.log('%c Style!', 'color: #fff; background: #e3e');
 
 // warning!
+console.warn('warn');
 
 // Error :|
+console.error('error');
 
 // Info
+console.info('info');
 
 // Testing
+console.assert(1 == 2, '1 is not equal to 2');
 
 // clearing
+console.clear();
 
 // Viewing DOM Elements
+console.dir(document.querySelector('html'));
 
 // Grouping together
+dogs.forEach((dog) => {
+  console.groupCollapsed(dog.name);
+  console.log('Age: ', dog.age);
+  console.groupEnd(dog.name);
+});
 
 // counting
+console.count('Count!');
+console.count('Count!');
+console.count('Count!');
+console.count('Count!');
+console.count('Count!');
 
 // timing
+console.time('time');
+const time1 = performance.now();
+setTimeout(() => {
+  console.timeEnd('time');
+  const time2 = performance.now();
+  console.log(`performance.now(): ${time2 - time1} ms`);
+}, 100);
