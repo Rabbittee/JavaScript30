657b0a2b081149c2fce6e0cf64e3797591bdfb56
Rabbittee <rabbitteetw@gmail.com>
2022-02-09 08:51:54 +0000

    Auto-merge feature/day08-husky

--- day08/Husky/main.ts
+++ day08/Husky/main.ts
@@ -64,6 +64,8 @@ class Canvas {
     });
     window.addEventListener('keyup', (e: KeyboardEvent) => {});
 
+    this.canvas.addEventListener('touchmove', (e) => e.preventDefault());
+
     this.canvas.addEventListener('pointerdown', (e: MouseEvent) => {
       this.updatePoint(e);
       this.drawFlag = true;
