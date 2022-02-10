2f5b1b8ed1359a414d7be5730c0627b4b1959546
zoeGuava <zoeguava@gmail.com>
2022-02-11 01:21:48 +0800

    feat: 🎸 add day10 pencil's solution

--- day10/pencil/main.js
+++ day10/pencil/main.js
@@ -1,1 +1,24 @@
-console.log('here is day10 main.js');
+const checkboxes = document.querySelectorAll('input[type=checkbox]');
+
+let lastChecked;
+
+const handleCheck = (e) => {
+  let inBetween = false;
+  if (e.target.checked && e.shiftKey) {
+    checkboxes.forEach((checkbox) => {
+      if (checkbox === e.target || checkbox === lastChecked) {
+        inBetween = !inBetween;
+      }
+
+      if (inBetween) {
+        checkbox.checked = true;
+      }
+    });
+  }
+
+  lastChecked = e.target;
+};
+
+checkboxes.forEach((checkbox) => {
+  checkbox.addEventListener('click', handleCheck);
+});
