5bc0af558d774e7636ec6b11075152b621d7e7aa
Rabbittee <rabbitteetw@gmail.com>
2022-02-10 09:58:38 +0000

    Auto-merge feature/day10-kevin

---
+++ day10/kevin/index.js
@@ -0,0 +1,44 @@
+const select = (selector) => document.querySelector(selector);
+const checkboxSelector = 'input[type="checkbox"]';
+
+function holdShiftToCheckCheckboxes(selector = '#checklist') {
+  const form = select(selector);
+  const checkboxes = [...form.querySelectorAll(checkboxSelector)];
+  let prevCheckedIndex = null;
+  let isPressShift = false;
+
+  function checkInRange(from = 0, to = 0) {
+    const minOne = Math.min(from, to);
+    const maxOne = Math.max(from, to);
+    checkboxes.forEach((checkbox, i) => {
+      if (i >= minOne && i <= maxOne) checkbox.checked = true;
+    });
+  }
+
+  function findCheckboxIndex(currentCheckbox) {
+    return checkboxes.findIndex((checkbox) => checkbox === currentCheckbox);
+  }
+
+  function toggleIsPressShift(forceState) {
+    isPressShift = !!forceState ?? !isPressShift;
+  }
+
+  form.addEventListener('change', (e) => {
+    const currentCheckbox = e.target.closest(checkboxSelector);
+    const currentCheckedIndex = findCheckboxIndex(currentCheckbox);
+    if (isPressShift && typeof prevCheckedIndex === 'number') {
+      checkInRange(currentCheckedIndex, prevCheckedIndex);
+    }
+    prevCheckedIndex = currentCheckedIndex;
+  });
+
+  document.addEventListener('keydown', ({ key }) => {
+    if (key === 'Shift') toggleIsPressShift(true);
+  });
+
+  document.addEventListener('keyup', ({ key }) => {
+    if (key === 'Shift') toggleIsPressShift(false);
+  });
+}
+
+holdShiftToCheckCheckboxes();
