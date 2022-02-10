3924e5a8eff4b631461fb65f381d0c0a2d35611a
HuskyHsu <schi.hsu@gmail.com>
2022-02-10 11:16:24 +0800

    style: 💄 prettier

--- day09/kirby/script.ts
+++ day09/kirby/script.ts
@@ -1,41 +1,41 @@
 const dogs = [
-  { name: "Snickers", age: 2 },
-  { name: "hugo", age: 8 },
+  { name: 'Snickers', age: 2 },
+  { name: 'hugo', age: 8 },
 ];
 
 function makeGreen() {
-  const p = document.querySelector("p");
-  p.style.color = "#BADA55";
-  p.style.fontSize = "50px";
+  const p = document.querySelector('p');
+  p.style.color = '#BADA55';
+  p.style.fontSize = '50px';
 }
 
 // clearing
 console.clear();
 
 // Regular
-console.log("hello");
+console.log('hello');
 
 // Interpolated
-console.log("Hello I am a %s string!", "💩");
+console.log('Hello I am a %s string!', '💩');
 
 // Styled
 console.log(
-  "%c I am some great text",
-  "font-size: 50px; background: red; text-shadow: 10px 10px 0 blue"
+  '%c I am some great text',
+  'font-size: 50px; background: red; text-shadow: 10px 10px 0 blue'
 );
 
 // warning!
-console.warn("OH NOOO");
+console.warn('OH NOOO');
 
 // Error :|
-console.error("Shit!");
+console.error('Shit!');
 
 // Info
-console.info("Crocodiles eat 3-4 people per year");
+console.info('Crocodiles eat 3-4 people per year');
 
 // Testing
-const p = document.querySelector("p");
-console.assert(p.classList.contains("ouch"), "That is wrong!");
+const p = document.querySelector('p');
+console.assert(p.classList.contains('ouch'), 'That is wrong!');
 
 // Viewing DOM Elements
 console.log(p);
@@ -51,25 +51,25 @@ dogs.forEach((dog) => {
 });
 
 // counting
-console.count("Wes");
-console.count("Wes");
-console.count("Steve");
-console.count("Steve");
-console.count("Wes");
-console.count("Steve");
-console.count("Wes");
-console.count("Steve");
-console.count("Steve");
-console.count("Steve");
-console.count("Steve");
-console.count("Steve");
+console.count('Wes');
+console.count('Wes');
+console.count('Steve');
+console.count('Steve');
+console.count('Wes');
+console.count('Steve');
+console.count('Wes');
+console.count('Steve');
+console.count('Steve');
+console.count('Steve');
+console.count('Steve');
+console.count('Steve');
 
 // timing
-console.time("fetching data");
-fetch("https://api.github.com/users/wesbos")
+console.time('fetching data');
+fetch('https://api.github.com/users/wesbos')
   .then((data) => data.json())
   .then((data) => {
-    console.timeEnd("fetching data");
+    console.timeEnd('fetching data');
     console.log(data);
   });
 
