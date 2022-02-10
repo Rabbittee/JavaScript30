a06e686e0906575ea58859a71dbf750d2e815c36
Rabbittee <rabbitteetw@gmail.com>
2022-02-10 03:31:40 +0000

    Auto-merge feature/day10_plusfive

--- day09/White/script.js
+++ day09/White/script.js
@@ -1,4 +1,7 @@
-const dogs = [{ name: 'Snickers', age: 2 }, { name: 'hugo', age: 8 }];
+const dogs = [
+  { name: 'Snickers', age: 2 },
+  { name: 'hugo', age: 8 },
+];
 
 function makeGreen() {
   const p = document.querySelector('p');
@@ -7,50 +10,53 @@ function makeGreen() {
 }
 
 // Regular
-console.log('Hello rabbits!')
+console.log('Hello rabbits!');
 
 // Interpolated
-console.log('Hello %s rabbit!', 'white')
+console.log('Hello %s rabbit!', 'white');
 
 // Styled
-console.log('%c This is a pencil', 'font-size: 50px; background: lightblue; text-shadow: 10px 10px 0 aliceblue')
+console.log(
+  '%c This is a pencil',
+  'font-size: 50px; background: lightblue; text-shadow: 10px 10px 0 aliceblue'
+);
 
 // warning!
-console.warn('Be care of Pencil!')
+console.warn('Be care of Pencil!');
 
 // Error :|
-console.error('Pencil is colorful!')
+console.error('Pencil is colorful!');
 
 // Info
-console.info('Info icon only in firefox.')
+console.info('Info icon only in firefox.');
 
 // Testing
-console.assert( 1 > 2, 'Are you kidding?')
-console.assert( 1 === 1, '這行不會印出來')
+console.assert(1 > 2, 'Are you kidding?');
+console.assert(1 === 1, '這行不會印出來');
 
 // clearing
 //console.clear()
 
 // Viewing DOM Elements
-console.dir(document.getElementsByTagName('p'))
+console.dir(document.getElementsByTagName('p'));
 
 // Grouping together
-dogs.forEach(dog => {
-    console.groupCollapsed(`${dog.name}`)
-    console.log(`My dog is ${dog.name}, he is ${dog.age} years old.`)
-    console.groupEnd()
-  })
+dogs.forEach((dog) => {
+  console.groupCollapsed(`${dog.name}`);
+  console.log(`My dog is ${dog.name}, he is ${dog.age} years old.`);
+  console.groupEnd();
+});
 // counting
-console.count('White')
-console.count('Husky')
-console.count('White')
-console.count('White')
-console.count('Husky')
-console.count('Pencil')
+console.count('White');
+console.count('Husky');
+console.count('White');
+console.count('White');
+console.count('Husky');
+console.count('Pencil');
 
 // timing
-console.time('wait')
+console.time('wait');
 setTimeout(() => {
-    console.log('wait me!')
-    console.timeEnd('wait')
-} , 2000)
+  console.log('wait me!');
+  console.timeEnd('wait');
+}, 2000);
