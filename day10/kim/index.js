52526958d30048297c4a55b558d53aae6476bf7c
Rabbittee <rabbitteetw@gmail.com>
2022-02-10 03:52:57 +0000

    Auto-merge feature/day10_kim

--- day10/kim/index.js
+++ day10/kim/index.js
@@ -3,21 +3,21 @@ const checkboxs = document.querySelectorAll('.inbox input[type="checkbox"]');
 let lastChecked;
 
 function handleCheckout(e) {
-    // Check if they had the shift key down
-    // And check that they are checking it
-    // console.log(e)
-    if (e.shiftKey && this.checked) {
-        let inBetween = false;
-        checkboxs.forEach((checkbox) => {
-            if (checkbox === this || checkbox === lastChecked) {
-                inBetween = !inBetween;
-            }
-            if (inBetween) {
-                checkbox.checked = true;
-            }
-        });
-    }
-    lastChecked = checkBoxes[index];
+  // Check if they had the shift key down
+  // And check that they are checking it
+  // console.log(e)
+  if (e.shiftKey && this.checked) {
+    let inBetween = false;
+    checkboxs.forEach((checkbox) => {
+      if (checkbox === this || checkbox === lastChecked) {
+        inBetween = !inBetween;
+      }
+      if (inBetween) {
+        checkbox.checked = true;
+      }
+    });
+  }
+  lastChecked = checkBoxes[index];
 }
 
 checkboxs.forEach((checkbox) => checkbox.addEventListener('click', handleCheckout));
