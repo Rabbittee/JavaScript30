e3909cf8317d49d3d7fc8e5967c5fd30ba92be3d
Kimberly <work223305@gmail.com>
2022-02-14 23:53:53 +0800

    fix: 🐛 pressedLength, and secretCodeLength become Variable
    pressedLength, and secretCodeLength become Variable

--- day12/kim/index.js
+++ day12/kim/index.js
@@ -1,25 +1,26 @@
 const pressed = [];
 const secretCode = 'kimkim';
 window.addEventListener('keyup', (e) => {
-  //   console.log(e.key);
   pressed.push(e.key);
+  const pressedLength = pressed.length;
+  const secretCodeLength = secretCode.length;
 
   // splice(start, deleteCount)
-  pressed.splice(-secretCode.length - 1, pressed.length - secretCode.length);
+  pressed.splice(-pressedLength - 1, pressedLength - secretCodeLength);
 
   // 拆解前半段: splice(start)
   console.log(
     '如果為負數，它將從數位的末尾開始',
-    -secretCode.length - 1,
+    -secretCodeLength - 1,
     '且length - 1 為陣列的最後一個'
   ); // -7
 
   // 拆解後半段: splice(deleteCount)
-  console.log('密碼長度為', secretCode.length); // 6
-  console.log('當前輸入幾個值', pressed.length); // 當前輸值為：輸入一次為 1, 輸入兩次為 2
+  console.log('密碼長度為', secretCodeLength); // 6
+  console.log('當前輸入幾個值', pressedLength); // 當前輸值為：輸入一次為 1, 輸入兩次為 2
   console.log(
     '如果為 或為負數，則不刪除任何元素, 否則刪除',
-    pressed.length - secretCode.length,
+    pressedLength - secretCodeLength,
     '個元素'
   );
 
