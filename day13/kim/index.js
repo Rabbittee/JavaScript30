307a34af3b0c99794ced139be8e3a7e3e388924e
kimimberlyyen <work223305@gmail.com>
2022-02-17 17:52:04 +0800

    fix: 🐛 prettier

--- day13/kim/index.js
+++ day13/kim/index.js
@@ -1,28 +1,29 @@
-const imgs = document.querySelectorAll('.slide-in')
+const imgs = document.querySelectorAll('.slide-in');
 
 function activeImage() {
   // 底部位置 = 視窗內高度+滑鼠移動Y軸
-  const windownBottom = window.scrollY + window.innerHeight
+  const windownBottom = window.scrollY + window.innerHeight;
 
-  imgs.forEach(height => {
+  imgs.forEach((height) => {
     // offsetTop: 元素的顶部内边距的距离
-    const distanceWithTop = height.offsetTop
-    // 
+    const distanceWithTop = height.offsetTop;
+    //
     if (windownBottom >= distanceWithTop) {
-      height.classList.add("active")
+      height.classList.add('active');
     } else if (windownBottom < distanceWithTop) {
-      height.classList.remove("active")
+      height.classList.remove('active');
     }
-  })
+  });
 }
 
-window.addEventListener('scroll', activeImage)
-window.addEventListener('scroll', debounce(activeImage))
+window.addEventListener('scroll', activeImage);
+window.addEventListener('scroll', debounce(activeImage));
 
 function debounce(func, wait = 20, immediate = true) {
   var timeout;
   return function () {
-    var context = this, args = arguments;
+    var context = this,
+      args = arguments;
     var later = function () {
       timeout = null;
       if (!immediate) func.apply(context, args);
