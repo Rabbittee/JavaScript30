714796eea138eec69965c5d8a0a1f7e755aa886a
kayac-chang <egg734631@gmail.com>
2022-02-17 17:35:12 +0800

    refactor: 💡 fix format

--- day17/kirby/script.ts
+++ day17/kirby/script.ts
@@ -1,24 +1,24 @@
 const select = (query: string) => document.querySelector(query);
-const strip = (text: string) => text.replace(/^(a |the |an )/i, "").trim();
+const strip = (text: string) => text.replace(/^(a |the |an )/i, '').trim();
 const Band = (name: string) => /*html */ `
     <li>${name}</li>
 `;
 
-select("#bands").innerHTML = [
-  "The Plot in You",
-  "The Devil Wears Prada",
-  "Pierce the Veil",
-  "Norma Jean",
-  "The Bled",
-  "Say Anything",
-  "The Midway State",
-  "We Came as Romans",
-  "Counterparts",
-  "Oh, Sleeper",
-  "A Skylit Drive",
-  "Anywhere But Here",
-  "An Old Dog",
+select('#bands').innerHTML = [
+  'The Plot in You',
+  'The Devil Wears Prada',
+  'Pierce the Veil',
+  'Norma Jean',
+  'The Bled',
+  'Say Anything',
+  'The Midway State',
+  'We Came as Romans',
+  'Counterparts',
+  'Oh, Sleeper',
+  'A Skylit Drive',
+  'Anywhere But Here',
+  'An Old Dog',
 ]
   .sort((a, b) => (strip(a) > strip(b) ? 1 : -1))
   .map(Band)
-  .join("");
+  .join('');
