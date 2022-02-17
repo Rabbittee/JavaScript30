a03ce232e7a8b9e7518d045da5b24ca652897205
kayac-chang <egg734631@gmail.com>
2022-02-17 17:43:28 +0800

    refactor: 💡 fix prettier format

--- day21/kirby/script.ts
+++ day21/kirby/script.ts
@@ -1,19 +1,19 @@
 const select = <T extends Element>(qs: string) => document.querySelector<T>(qs);
 
 type Fn<T, R> = (arg: T) => R;
-const tap = <T>(fn: Fn<T, void>) => (arg: T) => (fn(arg), arg);
+const tap =
+  <T>(fn: Fn<T, void>) =>
+  (arg: T) => (fn(arg), arg);
 const monad = <T>(value: T) => ({
   map: <R>(fn: Fn<T, R>) => monad(fn(value)),
   unwrap: () => value,
 });
 
 const renderSpeed = (data: GeolocationPosition) =>
-  (select(".speed-value").textContent = String(data.coords.speed));
+  (select('.speed-value').textContent = String(data.coords.speed));
 
 const updateArrow = (data: GeolocationPosition) =>
-  (select<SVGAElement>(
-    ".arrow"
-  ).style.transform = `rotate(${data.coords.heading}deg)`);
+  (select<SVGAElement>('.arrow').style.transform = `rotate(${data.coords.heading}deg)`);
 
 navigator.geolocation.watchPosition(
   (data) =>
