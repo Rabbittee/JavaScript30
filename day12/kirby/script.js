09a1ca597246979af89f00ebd89e347a8bd023d8
Hello Kayac <egg734631@gmail.com>
2022-02-17 15:52:41 +0800

    feat: 🎸 add day12 kirby (#54)

---
+++ day12/kirby/script.js
@@ -0,0 +1,21 @@
+var bingo = function () { return window["cornify_add"](); };
+var fixed = function (size) { return function (list) {
+    list = list.slice(0, size);
+    function append(item) {
+        if (list.length + 1 > size)
+            list.shift();
+        list.push(item);
+    }
+    return { append: append, list: list };
+}; };
+var word = "kirby";
+var buffer = fixed(word.length)([]);
+window.addEventListener("keypress", function (e) {
+    if (!(e instanceof KeyboardEvent))
+        return;
+    console.log(e.key);
+    buffer.append(e.key);
+    if (word === buffer.list.join(""))
+        return bingo();
+    console.log(buffer.list);
+});
