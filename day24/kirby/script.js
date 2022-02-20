102e588f4b210565e43808f8dd642f52daf6d7a6
kayac-chang <egg734631@gmail.com>
2022-02-21 01:14:33 +0800

    feat: 🎸 add day24 kirby

---
+++ day24/kirby/script.js
@@ -0,0 +1,30 @@
+var tap = function (fn) { return function (arg) { return (fn(arg), arg); }; };
+var select = function (query) {
+    return document.querySelector(query);
+};
+var selectAll = function (query) {
+    return Array.from(document.querySelectorAll(query));
+};
+function invariant(cond, message) {
+    if (cond)
+        return;
+    throw new Error(message);
+}
+// Sticky Change Observation
+var observer = new IntersectionObserver(function (entries) {
+    return entries.forEach(function (_a) {
+        var target = _a.target, isIntersecting = _a.isIntersecting;
+        return target.dispatchEvent(new CustomEvent("stick-change", { detail: { isIntersecting: isIntersecting } }));
+    });
+}, { threshold: 1 });
+selectAll("nav.sticky")
+    .map(tap(function (el) { return observer.observe(el); }))
+    .map(tap(function (el) {
+    return el.addEventListener("stick-change", function (_a) {
+        var target = _a.target, detail = _a.detail;
+        invariant(target instanceof HTMLElement, "target should be HTMLElement");
+        // see style detail in style.css
+        document.body.classList //
+            .toggle("active", !detail.isIntersecting);
+    });
+}));
---
+++ day24/kirby/script.ts
@@ -0,0 +1,51 @@
+type Fn<T, R> = (arg: T) => R;
+const tap = <T>(fn: Fn<T, any>) => (arg: T) => (fn(arg), arg);
+
+const select = <T extends Element>(query: string) =>
+  document.querySelector<T>(query);
+
+const selectAll = <T extends Element>(query: string) =>
+  Array.from(document.querySelectorAll<T>(query));
+
+function invariant(cond: any, message: string): asserts cond {
+  if (cond) return;
+
+  throw new Error(message);
+}
+
+// === utils end ===
+
+type StickChangeEvent = CustomEvent<{
+  isIntersecting: boolean;
+}>;
+interface ElementEventMap {
+  "stick-change": StickChangeEvent;
+}
+
+// Sticky Change Observation
+const observer = new IntersectionObserver(
+  (entries) =>
+    entries.forEach(({ target, isIntersecting }) =>
+      target.dispatchEvent(
+        new CustomEvent("stick-change", { detail: { isIntersecting } })
+      )
+    ),
+  { threshold: 1 }
+);
+
+selectAll("nav.sticky")
+  .map(tap((el) => observer.observe(el)))
+  .map(
+    tap((el) =>
+      el.addEventListener("stick-change", ({ target, detail }) => {
+        invariant(
+          target instanceof HTMLElement,
+          "target should be HTMLElement"
+        );
+
+        // see style detail in style.css
+        document.body.classList //
+          .toggle("active", !detail.isIntersecting);
+      })
+    )
+  );
