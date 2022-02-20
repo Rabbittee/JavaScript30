5c68afae5526b4bc3ce88785e1420d1c47f3826a
Rabbittee <rabbitteetw@gmail.com>
2022-02-20 12:24:13 +0000

    Auto-merge feature/day13_plusfive

--- day13/plusfive/main.js
+++ day13/plusfive/main.js
@@ -5,26 +5,26 @@
  * @LastEditTime:2022-02-20 20:13:19
  * @Description:
  */
-const selectAll = (query) => document.querySelectorAll(query)
+const selectAll = (query) => document.querySelectorAll(query);
 
 const slideImage = (img) => {
-  img.classList.add('active')
-}
+  img.classList.add('active');
+};
 
 const callback = (entries) => {
-  entries.forEach(entry => {
-    if(!entry.isIntersecting) return
+  entries.forEach((entry) => {
+    if (!entry.isIntersecting) return;
 
-    slideImage(entry.target)
-  })
-}
+    slideImage(entry.target);
+  });
+};
 
 // 建立 observer
 const observer = new IntersectionObserver(callback, {
   // root: container, // 原本設置成 <div class="site-wrap"> 但會導致所有的圖片在一開始就一起被載入
   rootMargin: '0px',
-  threshold: 0.5
-})
+  threshold: 0.5,
+});
 
 // 觀察每一張圖片
-selectAll(".slide-in").forEach((el) => observer.observe(el))
+selectAll('.slide-in').forEach((el) => observer.observe(el));
