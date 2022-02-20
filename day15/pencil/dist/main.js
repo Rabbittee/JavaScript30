de9b06842928eb0659bad6f9b3f7b4d835e1a8c9
zoeGuava <zoeguava@gmail.com>
2022-02-20 15:52:46 +0800

    feature: day15 TS initial project

---
+++ day15/pencil/dist/main.js
@@ -0,0 +1,6 @@
+"use strict";
+const addItems = document.querySelector('.add-items');
+const itemsList = document.querySelector('.plates');
+const items = [];
+console.log('test ts file is here');
+//# sourceMappingURL=main.js.map
---
+++ day15/pencil/src/main.ts
@@ -0,0 +1,5 @@
+const addItems: HTMLElement | null = document.querySelector('.add-items');
+const itemsList: HTMLElement | null = document.querySelector('.plates');
+const items: Array<any> = [];
+
+console.log('test ts file is here');
