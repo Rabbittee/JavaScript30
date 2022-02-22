928d311ed078c54d35b4d9981c43db417ffcdb65
zoeGuava <zoeGuava@gmail.com>
2022-02-22 13:36:06 +0800

    update: delete ts config and file

--- day15/pencil/dist/main.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-const addItems = document.querySelector('.add-items');
-const itemsList = document.querySelector('.plates');
-const items = [];
-console.log('test ts file is here');
-//# sourceMappingURL=main.js.map
