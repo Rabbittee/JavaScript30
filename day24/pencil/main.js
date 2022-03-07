2604cada00a2e0ac2ca772c6f44469dfce79282e
zoeGuava <zoeguava@gmail.com>
2022-03-08 01:17:10 +0800

    feat: 🎸 add day24 pencil's solution

--- day24/pencil/main.js
+++ day24/pencil/main.js
@@ -1,1 +1,15 @@
-console.log('day24 pencil main js');
+const select = (DOM) => document.querySelector(DOM);
+
+let topOfNav = select('#main').offsetTop;
+
+const scrollListener = () => {
+  if (window.scrollY >= topOfNav) {
+    document.body.style.paddingTop = select('#main').offsetHeight + 'px';
+    document.body.classList.add('fixed-nav');
+  } else {
+    document.body.classList.remove('fixed-nav');
+    document.body.style.paddingTop = 0;
+  }
+};
+
+window.addEventListener('scroll', scrollListener);
