ddb8e036821909134a2e544f0683453cc46af6c6
KimberlyYen <87320011+KimberlyYen@users.noreply.github.com>
2022-03-11 11:51:01 +0800

    fix: 🐛 惠惠爆裂查檢查

--- day25/kim/index.js
+++ day25/kim/index.js
@@ -1,10 +1,10 @@
-const divs = document.querySelectorAll('div')
-const button = document.querySelector('button')
+const divs = document.querySelectorAll('div');
+const button = document.querySelector('button');
 
 // bubbling
 function logText(e) {
-  console.log(this.classList.value)
-  e.stopPropagation() // stop bubbling!
+  console.log(this.classList.value);
+  e.stopPropagation(); // stop bubbling!
 }
 
 // capture
@@ -12,15 +12,15 @@ divs.forEach((div) =>
   div.addEventListener('click', logText, {
     capture: false,
     once: true,
-  }),
-)
+  })
+);
 
 button.addEventListener(
   'click',
   () => {
-    console.log('click')
+    console.log('click');
   },
   {
     once: true,
-  },
-)
+  }
+);
