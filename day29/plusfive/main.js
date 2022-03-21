68fb17c915668f15389f4b9db74468e29ff5b708
Claire Li <claire.li@singularinfinity.com.tw>
2022-03-21 23:36:03 +0800

    format

--- day29/plusfive/main.js
+++ day29/plusfive/main.js
@@ -5,43 +5,49 @@
  * @LastEditTime:2022-03-20 20:34:44
  * @Description:
  */
-var select = function (query) { return document.querySelector(query); };
-var selectEvery = function (query) { return document.querySelectorAll(query); };
+var select = function (query) {
+  return document.querySelector(query);
+};
+var selectEvery = function (query) {
+  return document.querySelectorAll(query);
+};
 var timer;
 function countdown(secs) {
+  display(secs);
+  timer = window.setInterval(function () {
+    if (secs === 1) clearInterval(timer);
+    secs--;
     display(secs);
-    timer = window.setInterval(function () {
-        if (secs === 1)
-            clearInterval(timer);
-        secs--;
-        display(secs);
-    }, 1000);
+  }, 1000);
 }
 function display(seconds) {
-    var hours = Math.floor(seconds / 3600);
-    var remainderSeconds = seconds % 3600;
-    var minutes = Math.floor(remainderSeconds / 60);
-    remainderSeconds = seconds % 60;
-    select('.display__time-left').innerText = "\n    ".concat(hours < 10 ? showTens(hours) : hours, ":").concat(minutes < 10 ? showTens(minutes) : minutes, ":").concat(remainderSeconds < 10 ? showTens(remainderSeconds) : remainderSeconds, "\n  ");
+  var hours = Math.floor(seconds / 3600);
+  var remainderSeconds = seconds % 3600;
+  var minutes = Math.floor(remainderSeconds / 60);
+  remainderSeconds = seconds % 60;
+  select('.display__time-left').innerText = '\n    '
+    .concat(hours < 10 ? showTens(hours) : hours, ':')
+    .concat(minutes < 10 ? showTens(minutes) : minutes, ':')
+    .concat(remainderSeconds < 10 ? showTens(remainderSeconds) : remainderSeconds, '\n  ');
 }
 function showTens(number) {
-    return '0' + number;
+  return '0' + number;
 }
 selectEvery('[data-time]').forEach(function (btn) {
-    btn.addEventListener('click', function (e) {
-        clearInterval(timer);
-        if (e !== null && e.target instanceof HTMLButtonElement) {
-            var targetSec = Number(e.target.dataset.time);
-            countdown(targetSec);
-        }
-    });
+  btn.addEventListener('click', function (e) {
+    clearInterval(timer);
+    if (e !== null && e.target instanceof HTMLButtonElement) {
+      var targetSec = Number(e.target.dataset.time);
+      countdown(targetSec);
+    }
+  });
 });
 select('#custom').addEventListener('submit', function (e) {
-    e.preventDefault();
-    clearInterval(timer);
-    if (e !== null && e.target[0] instanceof HTMLInputElement) {
-        var targetSec = Number(e.target[0].value) * 60;
-        countdown(targetSec);
-        e.target[0].value = '';
-    }
+  e.preventDefault();
+  clearInterval(timer);
+  if (e !== null && e.target[0] instanceof HTMLInputElement) {
+    var targetSec = Number(e.target[0].value) * 60;
+    countdown(targetSec);
+    e.target[0].value = '';
+  }
 });
--- day29/plusfive/main.ts
+++ day29/plusfive/main.ts
@@ -5,54 +5,55 @@
  * @LastEditTime:2022-03-21 23:27:17
  * @Description:
  */
-const select: Function = (query: string): HTMLElement => document.querySelector(query)
-const selectEvery: Function = (query: string): NodeList => document.querySelectorAll(query)
-let timer: number
+const select: Function = (query: string): HTMLElement => document.querySelector(query);
+const selectEvery: Function = (query: string): NodeList => document.querySelectorAll(query);
+let timer: number;
 
-
-function countdown (secs: number): void {
-  display(secs)
+function countdown(secs: number): void {
+  display(secs);
 
   timer = window.setInterval(() => {
-    if (secs === 1) clearInterval(timer)
+    if (secs === 1) clearInterval(timer);
 
-    secs--
+    secs--;
 
-    display(secs)
-  }, 1000)
+    display(secs);
+  }, 1000);
 }
 
 function display(seconds: number): void {
-  const hours: number = Math.floor(seconds / 3600)
-  let remainderSeconds: number = seconds % 3600
-  const minutes: number = Math.floor(remainderSeconds / 60)
-  remainderSeconds = seconds % 60
+  const hours: number = Math.floor(seconds / 3600);
+  let remainderSeconds: number = seconds % 3600;
+  const minutes: number = Math.floor(remainderSeconds / 60);
+  remainderSeconds = seconds % 60;
 
   select('.display__time-left').innerText = `
-    ${hours < 10 ? showTens(hours) : hours}:${minutes < 10 ? showTens(minutes) : minutes}:${remainderSeconds < 10 ? showTens(remainderSeconds) : remainderSeconds}
-  `
+    ${hours < 10 ? showTens(hours) : hours}:${minutes < 10 ? showTens(minutes) : minutes}:${
+    remainderSeconds < 10 ? showTens(remainderSeconds) : remainderSeconds
+  }
+  `;
 }
 
-function showTens (number: number): string {
-  return '0' + number
+function showTens(number: number): string {
+  return '0' + number;
 }
 
 selectEvery('[data-time]').forEach((btn) => {
   btn.addEventListener('click', (e: Event) => {
-    clearInterval(timer)
+    clearInterval(timer);
     if (e !== null && e.target instanceof HTMLButtonElement) {
-      const targetSec = Number(e.target.dataset.time)
-      countdown(targetSec)
+      const targetSec = Number(e.target.dataset.time);
+      countdown(targetSec);
     }
-  })
-})
+  });
+});
 
 select('#custom').addEventListener('submit', (e: Event) => {
-  e.preventDefault()
-  clearInterval(timer)
+  e.preventDefault();
+  clearInterval(timer);
   if (e !== null && e.target[0] instanceof HTMLInputElement) {
-    const targetSec = Number(e.target[0].value) * 60
-    countdown(targetSec)
-    e.target[0].value = ''
+    const targetSec = Number(e.target[0].value) * 60;
+    countdown(targetSec);
+    e.target[0].value = '';
   }
-})
+});
