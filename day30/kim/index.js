1756f064240efa70e42a38e90db63563d9def5fe
kimimberlyyen <work223305@gmail.com>
2022-03-23 01:03:11 +0800

    feat: 🎸 add day30 kim
    add day30 kim

--- day30/kim/index.js
+++ day30/kim/index.js
@@ -6,44 +6,44 @@ let timeUp = false;
 let score = 0;
 
 function randomTime(min, max) {
-    return Math.round(Math.random() * (max - min) + min);
+  return Math.round(Math.random() * (max - min) + min);
 }
 
 function randomHole(holes) {
-    const idx = Math.floor(Math.random() * holes.length);
-    const hole = holes[idx];
-    if (hole === lastHole) {
-        console.log('Ah nah thats the same one bud');
-        return randomHole(holes);
-    }
-    lastHole = hole;
-    return hole;
+  const idx = Math.floor(Math.random() * holes.length);
+  const hole = holes[idx];
+  if (hole === lastHole) {
+    console.log('Ah nah thats the same one bud');
+    return randomHole(holes);
+  }
+  lastHole = hole;
+  return hole;
 }
 
 function peep() {
-    const time = randomTime(200, 1000);
-    const hole = randomHole(holes);
-    hole.classList.add('up');
-    setTimeout(() => {
-        hole.classList.remove('up');
-        if (!timeUp) peep();
-        peep();
-    }, time);
+  const time = randomTime(200, 1000);
+  const hole = randomHole(holes);
+  hole.classList.add('up');
+  setTimeout(() => {
+    hole.classList.remove('up');
+    if (!timeUp) peep();
+    peep();
+  }, time);
 }
 
 function startGame() {
-    scoreBoard.textContent = 0;
-    timeUp = false;
-    score = 0;
-    peep();
-    setTimeout(() => timeUp = true, 10000)
+  scoreBoard.textContent = 0;
+  timeUp = false;
+  score = 0;
+  peep();
+  setTimeout(() => (timeUp = true), 10000);
 }
 
 function bonk(e) {
-    if (!e.isTrusted) return; // cheater!
-    score++;
-    this.parentNode.classList.remove('up');
-    scoreBoard.textContent = score;
+  if (!e.isTrusted) return; // cheater!
+  score++;
+  this.parentNode.classList.remove('up');
+  scoreBoard.textContent = score;
 }
 
-moles.forEach(mole => mole.addEventListener('click', bonk));
+moles.forEach((mole) => mole.addEventListener('click', bonk));
